d359e6d9a72b3d921097adb33d6e360e3af5af24
Luigi Ray-Montanez <luigi@codeforamerica.org>
2020-05-06 11:36:11 -0400

    Initial commit 🚀

---
+++ src/main/java/org/codeforamerica/shiba/ShibaApplication.java
@@ -0,0 +1,13 @@
+package org.codeforamerica.shiba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShibaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShibaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/codeforamerica/shiba/ShibaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codeforamerica.shiba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShibaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
