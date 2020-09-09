87c484e1ab1f4e577db376b531f907d86033b4c0
Ben Calegari <bcalegari@codeforamerica.org>
2020-09-09 14:50:59 -0400

    Resolve lombok warning

--- src/main/java/org/codeforamerica/shiba/pages/CountyEmailMap.java
+++ src/main/java/org/codeforamerica/shiba/pages/CountyEmailMap.java
@@ -1,10 +1,12 @@
 package org.codeforamerica.shiba.pages;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.codeforamerica.shiba.County;
 
 import java.util.HashMap;
 
 @Data
+@EqualsAndHashCode(callSuper=true)
 public class CountyEmailMap extends HashMap<County, String> {
 }
