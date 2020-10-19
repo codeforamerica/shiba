3c449ba20124c4032d48ec001175be2d0f63fd92
Ben Calegari <bcalegari@codeforamerica.org>
2020-10-19 17:13:46 -0400

    Fix flyway bug by implementing FlywayMigrationStrategy
    - Fix found here: https://github.com/spring-projects/spring-boot/issues/23514#issuecomment-700077457
    - This issue may be auto-resolved in future versions of spring-boot, so we could try removing this when we pull in the next spring upgrade.
    
    Co-authored-by: Mike Rotondo <mrotondo@codeforamerica.org>

---
+++ src/main/java/org/codeforamerica/shiba/FlywayMigrationStrategyImpl.java
@@ -0,0 +1,13 @@
+package org.codeforamerica.shiba;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FlywayMigrationStrategyImpl implements FlywayMigrationStrategy {
+    @Override
+    public void migrate(Flyway flyway) {
+        flyway.migrate();
+    }
+}
