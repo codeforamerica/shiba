26ec830dea88c5ca01d99a8d85f3f6c4c4d35b08
Ben Calegari <bcalegari@codeforamerica.org>
2020-11-20 13:40:26 -0500

    Revert "Added configuration for Wright County"
    This reverts commit f2576df26635fd670f2dac65155ffd6cf6a42928.
    
    - Removing Wright county temporarily as they're not yet ready.
    
    [#175821545]

--- src/main/java/org/codeforamerica/shiba/County.java
+++ src/main/java/org/codeforamerica/shiba/County.java
@@ -1,14 +1,13 @@
 package org.codeforamerica.shiba;
 
 public enum County {
-    Hennepin, Olmsted, Wabasha, Wright, Other;
+    Hennepin, Olmsted, Wabasha, Other;
 
     public static County valueFor(String county) {
         return switch (county) {
             case "Hennepin" -> Hennepin;
             case "Olmsted" -> Olmsted;
             case "Wabasha" -> Wabasha;
-            case "Wright" -> Wright;
             default -> Other;
         };
     }
