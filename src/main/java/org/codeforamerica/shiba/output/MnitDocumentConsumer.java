322a2b94aeca0a93f59fbef071faa4b968957ff2
Alex Gonzalez <spokenbird@gmail.com>
2021-01-04 13:10:24 -0800

    Implement Send CCAP to County
    [#175153061]
    
    Co-authored-by: Britney Epps <bepps@codeforamerica.org>
    Co-authored-by: Sree Prasad <sprasad@codeforamerica.org>

--- src/main/java/org/codeforamerica/shiba/output/MnitDocumentConsumer.java
+++ src/main/java/org/codeforamerica/shiba/output/MnitDocumentConsumer.java
@@ -7,7 +7,10 @@ import org.codeforamerica.shiba.output.pdf.PdfGenerator;
 import org.codeforamerica.shiba.output.xml.XmlGenerator;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 import static org.codeforamerica.shiba.output.DocumentType.CAF;
+import static org.codeforamerica.shiba.output.DocumentType.CCAP;
 import static org.codeforamerica.shiba.output.Recipient.CASEWORKER;
 
 @Component
@@ -29,7 +32,26 @@ public class MnitDocumentConsumer {
         Sentry.configureScope(scope -> {
             scope.setContexts("applicationId", application.getId());
         });
+
         mnitClient.send(pdfGenerator.generate(application.getId(), CAF, CASEWORKER), application.getCounty());
+        if (shouldSendCCAP(application)) {
+            mnitClient.send(pdfGenerator.generate(application.getId(), CCAP, CASEWORKER), application.getCounty());
+        }
         mnitClient.send(xmlGenerator.generate(application.getId(), CAF, CASEWORKER), application.getCounty());
     }
+
+    private boolean shouldSendCCAP(Application application) {
+        List<String> applicantPrograms = application.getApplicationData().getPagesData().safeGetPageInputValue("choosePrograms", "programs");
+        Boolean applicantHasCCAP = applicantPrograms.contains("CCAP");
+        Boolean hasHousehold = application.getApplicationData().getSubworkflows().containsKey("household");
+        Boolean householdHasCCAP = false;
+        if (hasHousehold) {
+            householdHasCCAP = application.getApplicationData().getSubworkflows().get("household").stream()
+                    .anyMatch(iteration ->
+                            iteration.getPagesData().safeGetPageInputValue("householdMemberInfo", "programs").contains("CCAP"));
+        }
+
+        return applicantHasCCAP || householdHasCCAP;
+    }
+
 }
--- src/test/java/org/codeforamerica/shiba/output/MnitDocumentConsumerTest.java
+++ src/test/java/org/codeforamerica/shiba/output/MnitDocumentConsumerTest.java
@@ -6,15 +6,18 @@ import org.codeforamerica.shiba.mnit.MnitEsbWebServiceClient;
 import org.codeforamerica.shiba.output.pdf.PdfGenerator;
 import org.codeforamerica.shiba.output.xml.XmlGenerator;
 import org.codeforamerica.shiba.pages.data.ApplicationData;
+import org.codeforamerica.shiba.pages.data.InputData;
 import org.codeforamerica.shiba.pages.data.PageData;
 import org.codeforamerica.shiba.pages.data.PagesData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.ZonedDateTime;
+import java.util.List;
 import java.util.Map;
 
 import static org.codeforamerica.shiba.output.DocumentType.CAF;
+import static org.codeforamerica.shiba.output.DocumentType.CCAP;
 import static org.codeforamerica.shiba.output.Recipient.CASEWORKER;
 import static org.mockito.Mockito.*;
 
@@ -84,5 +87,32 @@ class MnitDocumentConsumerTest {
 
         verify(mnitClient).send(pdfApplicationFile, County.Olmsted);
         verify(mnitClient).send(xmlApplicationFile, County.Olmsted);
+    }
+
+    @Test
+    void sendsTheCcapPdfIfTheApplicationHasCCAP() {
+        ApplicationFile pdfApplicationFile = new ApplicationFile("my pdf".getBytes(), "someFile.pdf");
+        when(pdfGenerator.generate(any(), eq(CCAP), any())).thenReturn(pdfApplicationFile);
+        ApplicationFile xmlApplicationFile = new ApplicationFile("my xml".getBytes(), "someFile.xml");
+        when(xmlGenerator.generate(any(), any(), any())).thenReturn(xmlApplicationFile);
+
+        ApplicationData applicationData = new ApplicationData();
+        PagesData pagesData = new PagesData();
+        PageData chooseProgramsPage = new PageData();
+
+        chooseProgramsPage.put("programs", InputData.builder().value(List.of("CCAP")).build());
+        pagesData.put("choosePrograms", chooseProgramsPage);
+        applicationData.setPagesData(pagesData);
+
+        Application application = Application.builder()
+                .id("someId")
+                .completedAt(ZonedDateTime.now())
+                .applicationData(applicationData)
+                .county(County.Olmsted)
+                .timeToComplete(null)
+                .build();
+        documentConsumer.process(application);
+
+        verify(mnitClient).send(pdfApplicationFile, County.Olmsted);
     }
 }
