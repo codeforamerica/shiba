94bc75ba360c6482c74d83ba0ff9fcbe5f10613e
Luigi Ray-Montanez <luigi@codeforamerica.org>
2021-01-11 13:54:55 -0500

    WIP: Unearned Income Frequency Mapper
    Co-authored-by: Britney Epps <bepps@codeforamerica.org>
    Co-authored-by: Alex Gonzalez <agonzalez@codeforamerica.org>

--- src/main/java/org/codeforamerica/shiba/output/applicationinputsmappers/UnearnedIncomeFrequencyInputsMapper.java
+++ src/main/java/org/codeforamerica/shiba/output/applicationinputsmappers/UnearnedIncomeFrequencyInputsMapper.java
@@ -4,24 +4,25 @@ import org.codeforamerica.shiba.application.Application;
 import org.codeforamerica.shiba.output.ApplicationInput;
 import org.codeforamerica.shiba.output.ApplicationInputType;
 import org.codeforamerica.shiba.output.Recipient;
-import org.codeforamerica.shiba.pages.data.InputData;
-import org.codeforamerica.shiba.pages.data.PageData;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
-
-import static org.codeforamerica.shiba.output.FullNameFormatter.getListOfSelectedFullNames;
 
 @Component
 public class UnearnedIncomeFrequencyInputsMapper implements ApplicationInputsMapper {
 
     @Override
     public List<ApplicationInput> map(Application application, Recipient recipient, SubworkflowIterationScopeTracker scopeTracker) {
-        return application.getApplicationData().getPageData("unearnedIncomeSources").keySet().stream()
-                .map(inputData -> new ApplicationInput("unearnedIncomeSources", inputData + "Frequency", List.of("Monthly"), ApplicationInputType.SINGLE_VALUE))
+        return application.getApplicationData().getPageData("unearnedIncomeSources").entrySet().stream()
+                .filter(inputData -> !inputData.getValue().getValue().isEmpty())
+                .map(inputData ->
+                        new ApplicationInput(
+                                "unearnedIncomeSources",
+                                inputData.getKey().replace("Amount", "Frequency"),
+                                List.of("Monthly"),
+                                ApplicationInputType.SINGLE_VALUE)
+                )
                 .collect(Collectors.toList());
     }
 }
