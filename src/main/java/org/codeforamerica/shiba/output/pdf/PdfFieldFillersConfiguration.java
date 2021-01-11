81e467fd770c653f22b23ca2f9120efe0d9073f2
Sree P <sprasad@codeforamerica.org>
2021-01-11 10:33:23 -0800

    Updates to caseworker pdf - Removed beginning disclaimers (headers) - Removed ending disclaimers (footers) - Split out "Authorize to share information.." page to include in client pdf and exclude from caseworker pdf - Split out "Perjury and General Declarations" page to include in both client and caseworker pdf (and maintain page ordering)
    [#176385737]
    
    Co-authored-by: Mike Rotondo <mrotondo@codeforamerica.org>

--- src/main/java/org/codeforamerica/shiba/output/pdf/PdfFieldFillersConfiguration.java
+++ src/main/java/org/codeforamerica/shiba/output/pdf/PdfFieldFillersConfiguration.java
@@ -42,14 +42,13 @@ public class PdfFieldFillersConfiguration {
     @Bean
     public PdfFieldFiller caseworkerCcapFiller(
             @Value("classpath:cover-pages.pdf") Resource coverPages,
-            @Value("classpath:ccap-headers.pdf") Resource ccapHeaders,
             @Value("classpath:ccap-body-caseworker-page1.pdf") Resource ccapPage1,
             @Value("classpath:ccap-body.pdf") Resource ccapBody,
-            @Value("classpath:ccap-footers.pdf") Resource ccapFooters,
+            @Value("classpath:ccap-body-perjury-and-general-declarations.pdf") Resource ccapDeclarations,
             @Value("classpath:LiberationSans-Regular.ttf") Resource font
     ) {
         return new PDFBoxFieldFiller(List.of(
-                coverPages, ccapHeaders, ccapPage1, ccapBody, ccapFooters
+                coverPages, ccapPage1, ccapBody, ccapDeclarations
         ), font);
     }
 
@@ -59,11 +58,13 @@ public class PdfFieldFillersConfiguration {
             @Value("classpath:ccap-headers.pdf") Resource ccapHeaders,
             @Value("classpath:ccap-body-client-page1.pdf") Resource ccapPage1,
             @Value("classpath:ccap-body.pdf") Resource ccapBody,
+            @Value("classpath:ccap-body-authorize-info-sharing.pdf") Resource ccapInfoSharing,
+            @Value("classpath:ccap-body-perjury-and-general-declarations.pdf") Resource ccapDeclarations,
             @Value("classpath:ccap-footers.pdf") Resource ccapFooters,
             @Value("classpath:LiberationSans-Regular.ttf") Resource font
     ) {
         return new PDFBoxFieldFiller(List.of(
-                coverPages, ccapHeaders, ccapPage1, ccapBody, ccapFooters
+                coverPages, ccapHeaders, ccapPage1, ccapBody, ccapInfoSharing, ccapDeclarations, ccapFooters
         ), font);
     }
 
