f396a93262d599f2a56e633b716a9f11399c2b52
Luigi Ray-Montanez <luigi@codeforamerica.org>
2021-02-17 15:30:14 -0500

    WIP

--- src/main/java/org/codeforamerica/shiba/County.java
+++ src/main/java/org/codeforamerica/shiba/County.java
@@ -1,13 +1,26 @@
 package org.codeforamerica.shiba;
 
 public enum County {
-    Hennepin, Olmsted, Wabasha, Wright, Other;
+    Anoka, Carver, Clay, Cook, Dodge, Hennepin, Morrison, Olmsted, OtterTail, Sherburne, Steele, StLouis, Wabasha,
+    Wadena, Waseca, Wright, Other;
 
     public static County valueFor(String county) {
         return switch (county) {
+            case "Anoka" -> Anoka;
+            case "Carver" -> Carver;
+            case "Clay" -> Clay;
+            case "Cook" -> Cook;
+            case "Dodge" -> Dodge;
             case "Hennepin" -> Hennepin;
+            case "Morrison" -> Morrison;
             case "Olmsted" -> Olmsted;
+            case "Otter Tail" -> OtterTail;
+            case "Sherburne" -> Sherburne;
+            case "Steele" -> Steele;
+            case "St. Louis" -> StLouis;
             case "Wabasha" -> Wabasha;
+            case "Wadena" -> Wadena;
+            case "Waseca" -> Waseca;
             case "Wright" -> Wright;
             default -> Other;
         };
