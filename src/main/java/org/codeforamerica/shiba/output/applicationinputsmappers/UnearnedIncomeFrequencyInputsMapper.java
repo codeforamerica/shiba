4a65c4faef48099dd2edc30834b34c0f83135228
Mike Rotondo <mrotondo@codeforamerica.org>
2021-02-22 10:56:47 -0800

    CCAP-specific unearned income sources (#91)
    [#176829419]
    
    Co-authored-by: Luigi Ray-Montanez <luigi@codeforamerica.org>
    Co-authored-by: John Bisek <john.bisek@state.mn.us>
    Co-authored-by: Britney Epps <bepps@codeforamerica.org>

--- src/main/java/org/codeforamerica/shiba/output/applicationinputsmappers/UnearnedIncomeFrequencyInputsMapper.java
+++ src/main/java/org/codeforamerica/shiba/output/applicationinputsmappers/UnearnedIncomeFrequencyInputsMapper.java
@@ -9,24 +9,28 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
 public class UnearnedIncomeFrequencyInputsMapper implements ApplicationInputsMapper {
 
     @Override
     public List<ApplicationInput> map(Application application, Recipient recipient, SubworkflowIterationScopeTracker scopeTracker) {
-        return Optional.ofNullable(application.getApplicationData().getPageData("unearnedIncomeSources"))
-                .map(pageData -> pageData
-                        .entrySet().stream()
-                        .filter(inputData -> !inputData.getValue().getValue().isEmpty())
-                        .map(inputData ->
-                                new ApplicationInput(
-                                        "unearnedIncomeSources",
-                                        inputData.getKey().replace("Amount", "Frequency"),
-                                        List.of("Monthly"),
-                                        ApplicationInputType.SINGLE_VALUE)
-                        )
-                        .collect(Collectors.toList()))
-                .orElse(List.of());
+        return List.of("unearnedIncomeSources", "unearnedIncomeSourcesCcap")
+                .stream()
+                .flatMap(pageName ->
+                        Optional.ofNullable(application.getApplicationData().getPageData(pageName))
+                        .map(pageData -> pageData
+                                .entrySet().stream()
+                                .filter(inputData -> !inputData.getValue().getValue().isEmpty())
+                                .map(inputData ->
+                                        new ApplicationInput(
+                                                pageName,
+                                                inputData.getKey().replace("Amount", "Frequency"),
+                                                List.of("Monthly"),
+                                                ApplicationInputType.SINGLE_VALUE)
+                                ))
+                        .orElse(Stream.of()))
+                .collect(Collectors.toList());
     }
 }
