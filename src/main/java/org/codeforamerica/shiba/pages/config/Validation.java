bb18636fde412b1c3c03bd8c5e754c9f0f22d0e4
britneyepps <bepps@codeforamerica.org>
2021-03-01 13:04:09 -0500

    [#177085114]
    Modified Money validator to allow commas

--- src/main/java/org/codeforamerica/shiba/pages/config/Validation.java
+++ src/main/java/org/codeforamerica/shiba/pages/config/Validation.java
@@ -24,7 +24,8 @@ public enum Validation {
             .contains(strings.get(0).toUpperCase())),
     PHONE(strings -> String.join("", strings).replaceAll("[^\\d]", "").matches("[2-9]\\d{9}")),
     PHONE_STARTS_WITH_ONE(strings -> !String.join("", strings).replaceAll("[^\\d]", "").startsWith("1")),
-    MONEY(strings -> String.join("", strings).matches("\\d+(\\.\\d{1,2})?")),
+    //MONEY(strings -> String.join("", strings).matches("\\d+(\\.\\d{1,2})?")),
+    MONEY(strings -> String.join("", strings).matches("^([0-9]{1,3},([0-9]{3},)*[0-9]{3}|[0-9]+)(.[0-9][0-9])?")),
     EMAIL(strings -> String.join("", strings).trim().matches("[a-zA-Z0-9!#$%&'*+=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&'*+=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?"));
 
     private final Predicate<List<String>> rule;
