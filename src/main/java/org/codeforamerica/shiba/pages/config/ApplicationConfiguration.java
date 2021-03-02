e031fe0927340138eddce1f9bf9655063137f60d
Alex Gonzalez <spokenbird@gmail.com>
2021-03-02 14:48:29 -0800

    Implement Document Recommendations Screen and Logic (#96)
    [#176912511]
    
    Co-authored-by: Luigi Ray-Montanez <luigi@codeforamerica.org>
    Co-authored-by: Britney Epps <bepps@codeforamerica.org>
    Co-authored-by: Mike Rotondo <mrotondo@codeforamerica.org>

--- src/main/java/org/codeforamerica/shiba/pages/config/ApplicationConfiguration.java
+++ src/main/java/org/codeforamerica/shiba/pages/config/ApplicationConfiguration.java
@@ -2,6 +2,7 @@ package org.codeforamerica.shiba.pages.config;
 
 import lombok.Data;
 import org.codeforamerica.shiba.inputconditions.Condition;
+import org.codeforamerica.shiba.output.CompositeCondition;
 
 import java.util.List;
 import java.util.Map;
@@ -9,6 +10,7 @@ import java.util.Map;
 @Data
 public class ApplicationConfiguration {
     private List<Condition> conditionDefinitions;
+    private List<CompositeCondition> compositeConditionDefinitions;
     private List<PageConfiguration> pageDefinitions;
     private LandmarkPagesConfiguration landmarkPages;
     private Map<String, PageWorkflowConfiguration> workflow;
--- src/main/java/org/codeforamerica/shiba/pages/config/PageDatasource.java
+++ src/main/java/org/codeforamerica/shiba/pages/config/PageDatasource.java
@@ -6,4 +6,5 @@ import lombok.Data;
 public class PageDatasource {
     private String pageName;
     private String groupName;
+    private boolean optional = false;
 }
