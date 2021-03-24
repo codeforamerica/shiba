af1b542d8b1ee08682fa4079f8bf00b8c8f1bea2
Alex Gonzalez <spokenbird@gmail.com>
2021-03-24 16:41:27 -0700

    WIP - Testing our S3DocumentUploadService works as intended - Need to figure out how to differentiate between the real bucket and test bucket
    [#177445044]

--- src/test/java/org/codeforamerica/shiba/documents/S3DocumentUploadServiceTest.java
+++ src/test/java/org/codeforamerica/shiba/documents/S3DocumentUploadServiceTest.java
@@ -1,5 +1,7 @@
 package org.codeforamerica.shiba.documents;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import org.codeforamerica.shiba.pages.data.UploadedDocument;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +9,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("demo")
 @Tag("externalServiceTest")
@@ -17,11 +22,18 @@ class S3DocumentUploadServiceTest {
     @Autowired
     private S3DocumentUploadService s3DocumentUploadService;
 
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    AmazonS3Client s3Client;
+
     @Value("classpath:shiba.jpg")
     private Resource shibaImg;
 
     @Test
-    void itActuallyUploads() throws IOException {
-        s3DocumentUploadService.upload("testshibaimg.jpg", shibaImg.getFile());
+    void itActuallyUploadsAndDeletes() throws IOException, InterruptedException {
+        s3DocumentUploadService.upload("testshibaimg.jpg", (MultipartFile) shibaImg.getFile());
+        assertThat(s3Client.listObjects().getObjectSummaries().size()).isEqualTo(1);
+        s3DocumentUploadService.delete("testshibaimg.jpg");
+        assertThat(s3Client.listObjects().getObjectSummaries().size()).isEqualTo(0);
     }
 }
