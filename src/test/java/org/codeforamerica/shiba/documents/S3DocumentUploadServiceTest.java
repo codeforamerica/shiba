3dd937cd1be4a02eba36551ab318b9eeb05c9099
Alex Gonzalez <spokenbird@gmail.com>
2021-03-24 17:20:01 -0700

    WIP - Update S3DocumentUploadServiceTest to include correct bucket name
    [#177445044]

--- src/test/java/org/codeforamerica/shiba/documents/S3DocumentUploadServiceTest.java
+++ src/test/java/org/codeforamerica/shiba/documents/S3DocumentUploadServiceTest.java
@@ -1,23 +1,26 @@
 package org.codeforamerica.shiba.documents;
 
 import com.amazonaws.services.s3.AmazonS3Client;
-import org.codeforamerica.shiba.pages.data.UploadedDocument;
+import org.apache.commons.io.IOUtils;
+import org.codeforamerica.shiba.configurations.TransferManagerConfiguration;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("demo")
 @Tag("externalServiceTest")
-@SpringBootTest
+@SpringBootTest(classes = {S3DocumentUploadService.class, AmazonS3Client.class, TransferManagerConfiguration.class})
 class S3DocumentUploadServiceTest {
     @Autowired
     private S3DocumentUploadService s3DocumentUploadService;
@@ -31,9 +34,12 @@ class S3DocumentUploadServiceTest {
 
     @Test
     void itActuallyUploadsAndDeletes() throws IOException, InterruptedException {
-        s3DocumentUploadService.upload("testshibaimg.jpg", (MultipartFile) shibaImg.getFile());
-        assertThat(s3Client.listObjects().getObjectSummaries().size()).isEqualTo(1);
+        FileInputStream input = new FileInputStream(shibaImg.getFile());
+        MultipartFile multipartFile = new MockMultipartFile("file",
+                shibaImg.getFile().getName(), "text/plain", IOUtils.toByteArray(input));
+        s3DocumentUploadService.upload("testshibaimg.jpg", multipartFile);
+        assertThat(s3Client.listObjects("documents-test-mnbenefits-org").getObjectSummaries().size()).isEqualTo(1);
         s3DocumentUploadService.delete("testshibaimg.jpg");
-        assertThat(s3Client.listObjects().getObjectSummaries().size()).isEqualTo(0);
+        assertThat(s3Client.listObjects("documents-test-mnbenefits-org").getObjectSummaries().size()).isEqualTo(0);
     }
 }
