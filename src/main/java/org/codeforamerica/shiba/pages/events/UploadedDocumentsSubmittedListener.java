616ce0625bc082742e340e491d302291ba962052
Andrew Edstrom <andrewedstrom@gmail.com>
2021-04-14 10:51:22 -0700

    Don't send uploaded docs to MNit if feature flag is off
    - We used the document-upload-feature feature flag.
    
    [#176421178]
    
    Co-authored-by: Alex Gonzalez <agonzalez@codeforamerica.org>

--- src/main/java/org/codeforamerica/shiba/pages/events/UploadedDocumentsSubmittedListener.java
+++ src/main/java/org/codeforamerica/shiba/pages/events/UploadedDocumentsSubmittedListener.java
@@ -5,6 +5,7 @@ import org.codeforamerica.shiba.MonitoringService;
 import org.codeforamerica.shiba.application.Application;
 import org.codeforamerica.shiba.application.ApplicationRepository;
 import org.codeforamerica.shiba.output.MnitDocumentConsumer;
+import org.codeforamerica.shiba.pages.config.FeatureFlagConfiguration;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
@@ -16,18 +17,22 @@ public class UploadedDocumentsSubmittedListener {
     private final MnitDocumentConsumer mnitDocumentConsumer;
     private final ApplicationRepository applicationRepository;
     private final MonitoringService monitoringService;
+    private final FeatureFlagConfiguration featureFlags;
 
-    public UploadedDocumentsSubmittedListener(MnitDocumentConsumer mnitDocumentConsumer, ApplicationRepository applicationRepository, MonitoringService monitoringService) {
+    public UploadedDocumentsSubmittedListener(MnitDocumentConsumer mnitDocumentConsumer, ApplicationRepository applicationRepository, MonitoringService monitoringService, FeatureFlagConfiguration featureFlags) {
         this.mnitDocumentConsumer = mnitDocumentConsumer;
         this.applicationRepository = applicationRepository;
         this.monitoringService = monitoringService;
+        this.featureFlags = featureFlags;
     }
 
     @Async
     @EventListener
     public void sendViaApi(UploadedDocumentsSubmittedEvent event) {
-        log.info("Processing uploaded documents");
-        mnitDocumentConsumer.processUploadedDocuments(getApplicationFromEvent(event));
+        if (featureFlags.get("document-upload-feature").isOn()) {
+            log.info("Processing uploaded documents");
+            mnitDocumentConsumer.processUploadedDocuments(getApplicationFromEvent(event));
+        }
     }
 
     @NotNull
