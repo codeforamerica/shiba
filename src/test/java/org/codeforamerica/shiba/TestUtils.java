e86cfa6117d8fe90b913fc5128b70ac1845589e0
Andrew Edstrom <andrewedstrom@gmail.com>
2021-04-27 11:37:02 -0700

    Create TestUtils
    - move both getAbsoluteFilepath methods there
    
    [#177843919]

---
+++ src/test/java/org/codeforamerica/shiba/TestUtils.java
@@ -0,0 +1,20 @@
+package org.codeforamerica.shiba;
+
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class TestUtils {
+    public static Path getAbsoluteFilepath(String resourceFilename) {
+        return Paths.get(getAbsoluteFilepathString(resourceFilename));
+    }
+
+    static String getAbsoluteFilepathString(String resourceFilename) {
+        URL resource = TestUtils.class.getClassLoader().getResource(resourceFilename);
+        if (resource != null) {
+        	return (new File(resource.getFile())).getAbsolutePath();
+        }
+        return "";
+    }
+}
