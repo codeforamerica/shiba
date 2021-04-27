7152be8803607266528c84d7b11b7926ef04ddb4
Andrew Edstrom <andrewedstrom@gmail.com>
2021-04-27 12:34:37 -0700

    Add setup and fixture explanation for test
    [#177843919]

--- src/test/java/org/codeforamerica/shiba/pages/data/ApplicationDataSerializationTest.java
+++ src/test/java/org/codeforamerica/shiba/pages/data/ApplicationDataSerializationTest.java
@@ -1,5 +1,6 @@
 package org.codeforamerica.shiba.pages.data;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
@@ -11,10 +12,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.codeforamerica.shiba.TestUtils.getAbsoluteFilepath;
 
 class ApplicationDataSerializationTest {
+    private byte[] serializedApplicationDataFromOldSession;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        /*
+         * This fixture is the value of spring_session_attributes.attribute_bytes for one session in the database.
+         *
+         * That session was created while running the code from this commit: 1088f79c08dab374ebe5d6fb8a113a5c779cf004
+         *
+         * The application corresponding to that session had made it all the way through the application process and
+         * was in the process of uploading documents when its attribute_bytes were captured in this fixture
+         */
+        serializedApplicationDataFromOldSession =  Files.readAllBytes(getAbsoluteFilepath("sessionApplicationDataFixture.txt"));
+    }
+
     @Test
-    void canBeDeserialized() throws IOException, ClassNotFoundException {
-        var sessionBytes = Files.readAllBytes(getAbsoluteFilepath("sessionApplicationDataFixture.txt"));
-        var applicationData = deserializeObjectFromByteArray(sessionBytes);
+    void maintainsBackwardCompatibilityWhenDeserializingOldSessions() throws IOException, ClassNotFoundException {
+        var applicationData = deserializeObjectFromByteArray(serializedApplicationDataFromOldSession);
 
         assertThat(applicationData).isInstanceOf(ApplicationData.class);
     }
