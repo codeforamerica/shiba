7e0880f88295720a94b4b083cb4992f4ab417204
Andrew Edstrom <andrewedstrom@gmail.com>
2021-04-27 11:45:23 -0700

    Add test for session deserialization
    - Fix validator serialVersionUID to match autogenerated ID
    
    [#177843919]

--- src/main/java/org/codeforamerica/shiba/pages/config/Validator.java
+++ src/main/java/org/codeforamerica/shiba/pages/config/Validator.java
@@ -8,7 +8,7 @@ import java.io.*;
 @Data
 public class Validator implements Serializable {
     @Serial
-    private static final long serialVersionUID = 6670512506564832396L;
+    private static final long serialVersionUID = -6670512506564832396L;
 
     private Validation validation = Validation.NONE;
     private String errorMessageKey;
---
+++ src/test/java/org/codeforamerica/shiba/pages/data/ApplicationDataSerializationTest.java
@@ -0,0 +1,27 @@
+package org.codeforamerica.shiba.pages.data;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.nio.file.Files;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.codeforamerica.shiba.TestUtils.getAbsoluteFilepath;
+
+class ApplicationDataSerializationTest {
+    @Test
+    void canBeDeserialized() throws IOException, ClassNotFoundException {
+        var sessionBytes = Files.readAllBytes(getAbsoluteFilepath("applicationDataBytes.txt"));
+        var applicationData = deserializeObjectFromByteArray(sessionBytes);
+
+        assertThat(applicationData).isInstanceOf(ApplicationData.class);
+    }
+
+    private Object deserializeObjectFromByteArray(byte[] b) throws IOException, ClassNotFoundException {
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(new ByteArrayInputStream(b))) {
+            return objectInputStream.readObject();
+        }
+    }
+}
