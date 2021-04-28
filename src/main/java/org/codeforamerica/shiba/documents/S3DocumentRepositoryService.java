cdc4ea3e29744ac3ad9132b260faae21d63ebfe1
britneyepps <bepps@codeforamerica.org>
2021-04-28 18:00:15 -0400

    Completed changes for release  [#177858644]
    Co-authored-by: Alex G <agonzalez@codeforamerica.org>

--- src/main/java/org/codeforamerica/shiba/documents/S3DocumentRepositoryService.java
+++ src/main/java/org/codeforamerica/shiba/documents/S3DocumentRepositoryService.java
@@ -9,6 +9,7 @@ import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.amazonaws.services.s3.transfer.TransferManager;
 import com.amazonaws.util.IOUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,7 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @Service
-//@Profile({"demo", "staging", "production"})
+@Profile({"demo", "staging", "production"})
 @Slf4j
 public class S3DocumentRepositoryService implements DocumentRepositoryService {
     private final TransferManager transferManager;
