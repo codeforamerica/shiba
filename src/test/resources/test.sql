db540190769a424dc2204166dcea565f83196535
Andrew Edstrom <andrewedstrom@gmail.com>
2021-04-28 09:57:18 -0700

    don't use flyway in tests

--- src/test/resources/test.sql
+++ src/test/resources/test.sql
@@ -1,1 +1,104 @@
 CREATE DOMAIN IF NOT EXISTS "JSONB" AS json;
+
+create table flyway_schema_history
+(
+    installed_rank integer                 not null
+        constraint flyway_schema_history_pk
+            primary key,
+    version        varchar(50),
+    description    varchar(200)            not null,
+    type           varchar(20)             not null,
+    script         varchar(1000)           not null,
+    checksum       integer,
+    installed_by   varchar(100)            not null,
+    installed_on   timestamp default now() not null,
+    execution_time integer                 not null,
+    success        boolean                 not null
+);
+
+create table applications
+(
+    id               varchar           not null
+        constraint applications_pkey
+            primary key,
+    completed_at     timestamp         not null,
+    county           varchar default 'OTHER':: character varying not null,
+    time_to_complete integer default 0 not null,
+    sentiment        varchar,
+    feedback         text,
+    flow             varchar,
+    application_data jsonb
+);
+
+create table research
+(
+    spoken_language          varchar,
+    written_language         varchar,
+    first_name               varchar,
+    last_name                varchar,
+    date_of_birth            date,
+    sex                      varchar,
+    phone_number             varchar,
+    email                    varchar,
+    phone_opt_in             boolean,
+    email_opt_in             boolean,
+    zip_code                 varchar,
+    snap                     boolean,
+    cash                     boolean,
+    housing                  boolean,
+    emergency                boolean,
+    has_household            boolean,
+    money_made_last30_days   numeric,
+    pay_rent_or_mortgage     boolean,
+    home_expenses_amount     numeric,
+    are_you_working          boolean,
+    self_employment          boolean,
+    social_security          boolean,
+    ssi                      boolean,
+    veterans_benefits        boolean,
+    unemployment             boolean,
+    workers_compensation     boolean,
+    retirement               boolean,
+    child_or_spousal_support boolean,
+    tribal_payments          boolean,
+    household_size           integer,
+    entered_ssn              boolean,
+    flow                     varchar,
+    application_id           varchar,
+    county                   varchar,
+    childcare                boolean
+);
+
+create table spring_session
+(
+    primary_id            char(36) not null
+        constraint spring_session_pk
+            primary key,
+    session_id            char(36) not null,
+    creation_time         bigint   not null,
+    last_access_time      bigint   not null,
+    max_inactive_interval integer  not null,
+    expiry_time           bigint   not null,
+    principal_name        varchar(100)
+);
+
+create unique index spring_session_ix1
+    on spring_session (session_id);
+
+create index spring_session_ix2
+    on spring_session (expiry_time);
+
+create index spring_session_ix3
+    on spring_session (principal_name);
+
+create table spring_session_attributes
+(
+    session_primary_id char(36)     not null
+        constraint spring_session_attributes_fk
+            references spring_session
+            on delete cascade,
+    attribute_name     varchar(200) not null,
+    attribute_bytes    bytea        not null,
+    constraint spring_session_attributes_pk
+        primary key (session_primary_id, attribute_name)
+);
