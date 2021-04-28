eeef97212c1796cb06dfb5442ed9917b3b5bd3c1
Andrew Edstrom <andrewedstrom@gmail.com>
2021-04-28 10:34:12 -0700

    add sequence

--- src/test/resources/test.sql
+++ src/test/resources/test.sql
@@ -1,20 +1,6 @@
 CREATE DOMAIN IF NOT EXISTS "JSONB" AS json;
 
-create table flyway_schema_history
-(
-    installed_rank integer                 not null
-        constraint flyway_schema_history_pk
-            primary key,
-    version        varchar(50),
-    description    varchar(200)            not null,
-    type           varchar(20)             not null,
-    script         varchar(1000)           not null,
-    checksum       integer,
-    installed_by   varchar(100)            not null,
-    installed_on   timestamp default now() not null,
-    execution_time integer                 not null,
-    success        boolean                 not null
-);
+CREATE SEQUENCE IF NOT EXISTS application_id START WITH 1 MAXVALUE 9999999;
 
 create table applications
 (
