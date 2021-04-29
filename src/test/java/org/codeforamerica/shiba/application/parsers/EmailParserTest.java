390349c64e40f15b26bcc6164fda97a3a5d88f7f
Andrew Edstrom <andrewedstrom@gmail.com>
2021-04-28 17:44:44 -0700

    introduce AbstractParserTest

--- src/test/java/org/codeforamerica/shiba/application/parsers/EmailParserTest.java
+++ src/test/java/org/codeforamerica/shiba/application/parsers/EmailParserTest.java
@@ -1,45 +1,28 @@
 package org.codeforamerica.shiba.application.parsers;
 
-import org.codeforamerica.shiba.YamlPropertySourceFactory;
 import org.codeforamerica.shiba.pages.data.ApplicationData;
 import org.codeforamerica.shiba.pages.data.InputData;
 import org.codeforamerica.shiba.pages.data.PageData;
 import org.codeforamerica.shiba.pages.data.PagesData;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.NONE;
 
-@SpringBootTest(webEnvironment = NONE)
-@ActiveProfiles("test")
-class EmailParserTest {
-    @Autowired
+class EmailParserTest extends AbstractParserTest {
     EmailParser emailParser;
-
-    @TestConfiguration
-    @PropertySource(value = "classpath:test-parsing-config.yaml", factory = YamlPropertySourceFactory.class)
-    static class TestPageConfiguration {
-        @Bean
-        @SuppressWarnings("ConfigurationProperties")
-        @ConfigurationProperties(prefix = "test-parsing")
-        public ParsingConfiguration parsingConfiguration() {
-            return new ParsingConfiguration();
-        }
-    }
 
     ApplicationData applicationData = new ApplicationData();
     PagesData pagesData = new PagesData();
     PageData contactInfo = new PageData();
+
+    @BeforeEach
+    void setUp() {
+        emailParser = new EmailParser(parsingConfiguration);
+    }
 
     @Test
     void shouldParseEmail() {
--- src/test/java/org/codeforamerica/shiba/application/parsers/MailingAddressParserTest.java
+++ src/test/java/org/codeforamerica/shiba/application/parsers/MailingAddressParserTest.java
@@ -1,44 +1,20 @@
 package org.codeforamerica.shiba.application.parsers;
 
-import org.codeforamerica.shiba.YamlPropertySourceFactory;
 import org.codeforamerica.shiba.pages.data.ApplicationData;
 import org.codeforamerica.shiba.pages.data.InputData;
 import org.codeforamerica.shiba.pages.data.PageData;
 import org.codeforamerica.shiba.pages.data.PagesData;
 import org.codeforamerica.shiba.pages.enrichment.Address;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.NONE;
 
-@SpringBootTest(webEnvironment = NONE)
-@ActiveProfiles("test")
-class MailingAddressParserTest {
-    @Autowired
-    MailingAddressParser mailingAddressParser;
-
-    @TestConfiguration
-    @PropertySource(value = "classpath:test-parsing-config.yaml", factory = YamlPropertySourceFactory.class)
-    static class TestPageConfiguration {
-        @SuppressWarnings("ConfigurationProperties")
-        @Bean
-        @ConfigurationProperties(prefix = "test-parsing")
-        public ParsingConfiguration parsingConfiguration() {
-            return new ParsingConfiguration();
-        }
-    }
-
+class MailingAddressParserTest extends AbstractParserTest {
     @Test
     void shouldParseApplicationData() {
+        MailingAddressParser mailingAddressParser = new MailingAddressParser(parsingConfiguration);
         ApplicationData applicationData = new ApplicationData();
         PagesData pagesData = new PagesData();
         PageData homePageData = new PageData();
