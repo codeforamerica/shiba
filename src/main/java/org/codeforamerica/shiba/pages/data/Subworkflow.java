004318132523a4612c9edd72cc35198c4446e5a3
Ben Calegari <bcalegari@codeforamerica.org>
2021-05-04 19:26:20 -0400

    Refactor Subworkflows to accept list of PagesData as a constructor
    [#177655642]

--- src/main/java/org/codeforamerica/shiba/pages/data/Subworkflow.java
+++ src/main/java/org/codeforamerica/shiba/pages/data/Subworkflow.java
@@ -2,15 +2,22 @@ package org.codeforamerica.shiba.pages.data;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.io.Serial;
 import java.util.ArrayList;
+import java.util.List;
 
+@NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class Subworkflow extends ArrayList<Iteration> {
     @Serial
     private static final long serialVersionUID = -8623969544656997967L;
+
+    public Subworkflow(List<PagesData> pagesData) {
+        pagesData.forEach(this::add);
+    }
 
     public void add(PagesData pagesData) {
         add(new Iteration(pagesData));
