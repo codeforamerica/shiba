31abdbf525b53a1a9db75a6c00b15978dcacc63b
Andrew Edstrom <andrewedstrom@gmail.com>
2021-05-12 12:59:11 -0700

    Create internationalization package + LocaleSpecificMessageSource
    [#178115548]

---
+++ src/main/java/org/codeforamerica/shiba/internationalization/InternationalizationUtils.java
@@ -0,0 +1,26 @@
+package org.codeforamerica.shiba.internationalization;
+
+import java.util.List;
+
+public class InternationalizationUtils {
+    /**
+     * Takes a list of strings and returns an English- or Spanish-language string representation of that list
+     * e.g. ["a", "b", "c"] -> "a, b and c"
+     * <p>
+     * The Oxford comma is omitted for Spanish-language compatibility. Spanish does not use the Oxford comma.
+     * <p>
+     * If we ever add a language which represents lists differently, this method will need to be updated.
+     */
+    public static String listToString(List<String> list, LocaleSpecificMessageSource lms) {
+        if (list.isEmpty()) return "";
+
+        int lastIdx = list.size() - 1;
+        String lastElement = list.get(lastIdx);
+        if (list.size() == 1) return lastElement;
+
+        String firstPart = String.join(", ", list.subList(0, lastIdx));
+        String and = lms.getMessage("general.and");
+
+        return String.join(" %s ".formatted(and), firstPart, lastElement);
+    }
+}
