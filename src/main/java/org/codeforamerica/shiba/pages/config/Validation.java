9beb60aaa852fa344ebd2776e2e11c98710760a2
Luigi Ray-Montanez <luigi@codeforamerica.org>
2021-05-18 18:04:55 -0400

    "Select your county" initial dropdown value for Later Docs
    [#178058692]
    
    Co-authored-by: Eric M. Johnson <eric.m.johnson@state.mn.us>

--- src/main/java/org/codeforamerica/shiba/pages/config/Validation.java
+++ src/main/java/org/codeforamerica/shiba/pages/config/Validation.java
@@ -27,7 +27,8 @@ public enum Validation {
     PHONE(strings -> String.join("", strings).replaceAll("[^\\d]", "").matches("[2-9]\\d{9}")),
     PHONE_STARTS_WITH_ONE(strings -> !String.join("", strings).replaceAll("[^\\d]", "").startsWith("1")),
     MONEY(strings -> String.join("", strings).matches("^(\\d{1,3},(\\d{3},)*\\d{3}|\\d+)(\\.\\d{1,2})?")),
-    EMAIL(strings -> String.join("", strings).trim().matches("[a-zA-Z0-9!#$%&'*+=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&'*+=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?"));
+    EMAIL(strings -> String.join("", strings).trim().matches("[a-zA-Z0-9!#$%&'*+=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&'*+=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?")),
+    COUNTY(strings -> !String.join("", strings).equals("SelectYourCounty"));
 
     private final Predicate<List<String>> rule;
 
