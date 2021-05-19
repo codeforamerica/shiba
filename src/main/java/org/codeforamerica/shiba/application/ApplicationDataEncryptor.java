7e8fc49d753f92c84c198106a757e5edcaaec670
Sree P <sprasad@codeforamerica.org>
2021-05-19 09:28:45 -0700

    Replacing the page data input object instead of overwriting - avoiding issue where application is encrypted when it's saved to DB but we want the unencrypted values in the session to validate and display to the client in some cases

--- src/main/java/org/codeforamerica/shiba/application/ApplicationDataEncryptor.java
+++ src/main/java/org/codeforamerica/shiba/application/ApplicationDataEncryptor.java
@@ -4,9 +4,13 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.codeforamerica.shiba.pages.data.ApplicationData;
+import org.codeforamerica.shiba.pages.data.InputData;
+import org.codeforamerica.shiba.pages.data.PageData;
+import org.codeforamerica.shiba.pages.data.PagesData;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.function.Function;
 
 @Component
@@ -46,27 +50,33 @@ public class ApplicationDataEncryptor implements Encryptor<ApplicationData> {
     }
 
     private void runCryptographicFunctionOnData(Function<String, String> encryptFunc, ApplicationData applicationData) {
-        String applicantSSN = applicationData.getPagesData().getPageInputFirstValue("personalInfo", "ssn");
-        if (applicantSSN != null && !applicantSSN.isBlank()) {
-            String encryptedApplicantSSN = encryptFunc.apply(applicantSSN);
-            applicationData.getPagesData().getPage("personalInfo").get("ssn").setValue(encryptedApplicantSSN, 0);
-        }
+        PagesData pagesData = applicationData.getPagesData();
 
-        applicantSSN = applicationData.getPagesData().getPageInputFirstValue("matchInfo", "ssn");
-        if (applicantSSN != null && applicantSSN.matches("\\d{3}-\\d{2}-\\d{4}")) {
-            String encryptedApplicantSSN = encryptFunc.apply(applicantSSN);
-            applicationData.getPagesData().getPage("matchInfo").get("ssn").setValue(encryptedApplicantSSN, 0);
-        }
+        setEncryptedSSNValue(encryptFunc, pagesData, "personalInfo");
+        setEncryptedSSNValue(encryptFunc, pagesData, "matchInfo");
 
         boolean hasHousehold = applicationData.getSubworkflows().containsKey("household");
         if (hasHousehold) {
-            applicationData.getSubworkflows().get("household").forEach(iteration -> {
-                String houseHoldMemberSSN = iteration.getPagesData().getPageInputFirstValue("householdMemberInfo", "ssn");
-                if (houseHoldMemberSSN != null && !houseHoldMemberSSN.isBlank()) {
-                    String encryptedHouseholdMemberSSN = encryptFunc.apply(houseHoldMemberSSN);
-                    iteration.getPagesData().getPage("householdMemberInfo").get("ssn").setValue(encryptedHouseholdMemberSSN, 0);
-                }
-            });
+            applicationData.getSubworkflows().get("household")
+                    .forEach(iteration -> setEncryptedSSNValue(encryptFunc, iteration.getPagesData(), "householdMemberInfo"));
+        }
+    }
+
+    /**
+     * Run encrypt function on value and replace PageData with the resulting value.
+     *
+     * @param encryptFunc encrypt or decrypt
+     * @param pagesData   pages data to get ssn
+     * @param pageName    page to replace
+     */
+    private void setEncryptedSSNValue(Function<String, String> encryptFunc, PagesData pagesData, String pageName) {
+        String applicantSSN = pagesData.getPageInputFirstValue(pageName, "ssn");
+        if (applicantSSN != null && !applicantSSN.isBlank()) {
+            String encryptedApplicantSSN = encryptFunc.apply(applicantSSN);
+            PageData encryptedPage = new PageData();
+            encryptedPage.putAll(pagesData.getPage(pageName));
+            encryptedPage.put("ssn", new InputData(List.of(encryptedApplicantSSN)));
+            pagesData.putPage(pageName, encryptedPage);
         }
     }
 
