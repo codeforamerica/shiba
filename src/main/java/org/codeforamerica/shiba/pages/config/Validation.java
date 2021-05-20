76ead63967706a05d3de04b11db99474d4f8a4d6
Sree P <sprasad@codeforamerica.org>
2021-05-20 10:52:02 -0700

    Adding number back-end validation for hours-per-week on job page.
    [#177875862]
    
    Co-authored-by: Luigi Ray-Montanez <luigi@codeforamerica.org>
    Co-authored-by: Britney Epps <bepps@codeforamerica.org>

--- src/main/java/org/codeforamerica/shiba/pages/config/Validation.java
+++ src/main/java/org/codeforamerica/shiba/pages/config/Validation.java
@@ -27,6 +27,7 @@ public enum Validation {
     PHONE(strings -> String.join("", strings).replaceAll("[^\\d]", "").matches("[2-9]\\d{9}")),
     PHONE_STARTS_WITH_ONE(strings -> !String.join("", strings).replaceAll("[^\\d]", "").startsWith("1")),
     MONEY(strings -> String.join("", strings).matches("^(\\d{1,3},(\\d{3},)*\\d{3}|\\d+)(\\.\\d{1,2})?")),
+    NUMBER(strings -> strings.get(0).trim().matches("\\d*")),
     EMAIL(strings -> String.join("", strings).trim().matches("[a-zA-Z0-9!#$%&'*+=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&'*+=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?")),
     COUNTY(strings -> !String.join("", strings).equals("SelectYourCounty"));
 
