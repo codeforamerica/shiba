929e1366778a42907a761a45a9d26cf568f6e384
britneyepps <bepps@codeforamerica.org>
2021-05-20 18:09:06 -0400

    Utilized existing monthly mapper and renamed it [#178139018]

--- src/main/java/org/codeforamerica/shiba/output/applicationinputsmappers/MonthlyFrequencyInputsMapper.java
+++ src/main/java/org/codeforamerica/shiba/output/applicationinputsmappers/MonthlyFrequencyInputsMapper.java
@@ -5,18 +5,19 @@ import org.codeforamerica.shiba.output.ApplicationInput;
 import org.codeforamerica.shiba.output.ApplicationInputType;
 import org.codeforamerica.shiba.output.Document;
 import org.codeforamerica.shiba.output.Recipient;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class MedicalExpensesFrequencyInputsMapper implements ApplicationInputsMapper {
-
+@Component
+public class MonthlyFrequencyInputsMapper implements ApplicationInputsMapper {
 
     @Override
     public List<ApplicationInput> map(Application application, Document document, Recipient recipient, SubworkflowIterationScopeTracker scopeTracker) {
-        return List.of("medicalExpensesSources")
+        return List.of("unearnedIncomeSources", "unearnedIncomeSourcesCcap", "medicalExpensesSources")
                 .stream()
                 .flatMap(pageName ->
                         Optional.ofNullable(application.getApplicationData().getPageData(pageName))
@@ -26,11 +27,11 @@ public class MedicalExpensesFrequencyInputsMapper implements ApplicationInputsMa
                                         .map(inputData ->
                                                 new ApplicationInput(
                                                         pageName,
-                                                        inputData.getKey().replace("Amount", "Frequency"),
-                                                        List.of("Monthly"),
-                                                        ApplicationInputType.SINGLE_VALUE)
-                                        ))
-                                .orElse(Stream.of()))
+                                                inputData.getKey().replace("Amount", "Frequency"),
+                                                List.of("Monthly"),
+                                                ApplicationInputType.SINGLE_VALUE)
+                                ))
+                        .orElse(Stream.of()))
                 .collect(Collectors.toList());
     }
 }
