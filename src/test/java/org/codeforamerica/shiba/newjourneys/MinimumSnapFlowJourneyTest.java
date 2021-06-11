a143fd1fadcf2fc3bf2379158f587e53590e7a7d
Andrew Edstrom <andrewedstrom@gmail.com>
2021-06-11 16:32:44 -0700

    Extract helper methods for MinimumSnapFlowJourneyTest
    [#178367502]

--- src/test/java/org/codeforamerica/shiba/newjourneys/MinimumSnapFlowJourneyTest.java
+++ src/test/java/org/codeforamerica/shiba/newjourneys/MinimumSnapFlowJourneyTest.java
@@ -28,121 +28,50 @@ import static org.mockito.Mockito.when;
 public class MinimumSnapFlowJourneyTest extends JourneyTest {
     private PDAcroForm caf;
 
+    private final String firstName = "Ahmed";
+    private final String lastName = "St. George";
+    private final String otherName = "defaultOtherName";
+    private final String dateOfBirth = "01/12/1928";
+    private final String sex = "Female";
+    private final String moveDate = "10/20/1993";
+    private final String previousCity = "Chicago";
+    private final String needsInterpreter = "Yes";
+    private final String email = "some@email.com";
+    private final String homeZip = "12345";
+    private final String homeCity = "someCity";
+    private final String homeStreetAddress = "someStreetAddress";
+    private final String homeApartmentNumber = "someApartmentNumber";
+    private final String mailingStreetAddress = "smarty street";
+    private final String mailingCity = "Cooltown";
+    private final String mailingState = "CA";
+    private final String mailingZip = "03104";
+    private final String mailingApartmentNumber = "";
+    private final String additionalInfo = "Some additional information about my application";
+    private final String caseNumber = "654321";
+    private final String signature = "some signature";
+
     @Test
-    void minimumFlowForSnapOnly() {
-        testPage.clickButton("Apply now");
-        testPage.clickContinue();
-
-        // Language Preferences
-        testPage.enter("writtenLanguage", "English");
-        testPage.enter("spokenLanguage", "English");
-        String needsInterpreter = "Yes";
-        testPage.enter("needInterpreter", needsInterpreter);
-        testPage.clickContinue();
-
-        // Program Selection
-        testPage.enter("programs", PROGRAM_SNAP);
-        testPage.clickContinue();
-        testPage.clickContinue();
-
-        // Personal Info
-        String firstName = "Ahmed";
-        String lastName = "St. George";
-        String otherName = "defaultOtherName";
-        String dateOfBirth = "01/12/1928";
-        String ssn = "123456789";
-        String sex = "Female";
-        String moveDate = "10/20/1993";
-        String previousCity = "Chicago";
-        testPage.enter("firstName", firstName);
-        testPage.enter("lastName", lastName);
-        testPage.enter("otherName", otherName);
-        testPage.enter("dateOfBirth", dateOfBirth);
-        testPage.enter("ssn", ssn);
-        testPage.enter("maritalStatus", "Never married");
-        testPage.enter("sex", sex);
-        testPage.enter("livedInMnWholeLife", "Yes");
-        testPage.enter("moveToMnDate", moveDate);
-        testPage.enter("moveToMnPreviousCity", previousCity);
-        testPage.clickContinue();
-
-        // How can we get in touch with you?
-        String phoneNumber = "7234567890";
-        String email = "some@email.com";
-        testPage.enter("phoneNumber", phoneNumber);
-        testPage.enter("email", email);
-        testPage.enter("phoneOrEmail", "Text me");
-        testPage.clickContinue();
-
-        // Where are you currently Living?
-        String homeZip = "12345";
-        String homeCity = "someCity";
-        String homeStreetAddress = "someStreetAddress";
-        String homeApartmentNumber = "someApartmentNumber";
-        testPage.enter("zipCode", homeZip);
-        testPage.enter("city", homeCity);
-        testPage.enter("streetAddress", homeStreetAddress);
-        testPage.enter("apartmentNumber", homeApartmentNumber);
-        testPage.enter("isHomeless", "I don't have a permanent address");
-        testPage.enter("sameMailingAddress", "No, use a different address for mail");
-        testPage.clickContinue();
-        testPage.clickButton("Use this address");
-
-        // Where can the county send your mail? (accept the smarty streets enriched address)
-        testPage.enter("zipCode", "23456");
-        testPage.enter("city", "someCity");
-        testPage.enter("streetAddress", "someStreetAddress");
-        testPage.enter("state", "IL");
-        testPage.enter("apartmentNumber", "someApartmentNumber");
-        String mailingStreetAddress = "smarty street";
-        String mailingCity = "Cooltown";
-        String mailingState = "CA";
-        String mailingZip = "03104";
-        String mailingApartmentNumber = "";
-        String mailingCounty = "someCounty";
-        when(smartyStreetClient.validateAddress(any())).thenReturn(
-                Optional.of(new Address(mailingStreetAddress, mailingCity, mailingState, mailingZip, mailingApartmentNumber, mailingCounty))
-        );
-        testPage.clickContinue();
-        testPage.clickElementById("enriched-address");
-        testPage.clickContinue();
-
-        // Let's review your info
-        assertThat(driver.findElementById("mailing-address_street").getText()).isEqualTo(mailingStreetAddress);
+    void nonExpeditedFlow() {
+        getToMinimumFlow();
 
         // Minimum flow, don't answer expedited questions
-        testPage.clickLink("Submit application now with only the above information.");
         testPage.clickLink("Finish application now");
 
-        assertThat(testPage.getTitle()).isEqualTo("Additional Info");
-        String additionalInfo = "Some additional information about my application";
-        driver.findElement(By.id("additionalInfo")).sendKeys(additionalInfo);
-        String caseNumber = "654321";
-        testPage.enter("caseNumber", caseNumber);
-        testPage.clickContinue();
+        String applicationId = finishApplicationAndDownloadCaf();
+        assertCafContainsAllFieldsForMinimumSnapFlow(applicationId);
+        assertApplicationSubmittedEventWasPublished(applicationId);
+    }
 
-        assertThat(testPage.getTitle()).isEqualTo("Legal Stuff");
-        testPage.enter("agreeToTerms", "I agree");
-        testPage.enter("drugFelony", NO.getDisplayValue());
-        testPage.clickContinue();
-        String signature = "some signature";
-        testPage.enter("applicantSignature", signature);
-        testPage.clickButton("Submit");
+    private void assertApplicationSubmittedEventWasPublished(String applicationId) {
+        ArgumentCaptor<ApplicationSubmittedEvent> captor = ArgumentCaptor.forClass(ApplicationSubmittedEvent.class);
+        verify(pageEventPublisher).publish(captor.capture());
+        ApplicationSubmittedEvent applicationSubmittedEvent = captor.getValue();
+        assertThat(applicationSubmittedEvent.getFlow()).isEqualTo(MINIMUM);
+        assertThat(applicationSubmittedEvent.getApplicationId()).isEqualTo(applicationId);
+        assertThat(applicationSubmittedEvent.getLocale()).isEqualTo(ENGLISH);
+    }
 
-        // No document upload
-        testPage.clickButton("Skip this for now");
-
-        // Download CAF
-        SuccessPage successPage = new SuccessPage(driver);
-        assertThat(successPage.CAFdownloadPresent()).isTrue();
-        assertThat(successPage.CCAPdownloadPresent()).isFalse();
-        successPage.downloadPdfs();
-        await().until(() -> getAllFiles().size() == successPage.pdfDownloadLinks());
-        caf = getAllFiles().get(CAF);
-        String applicationId = successPage.getConfirmationNumber();
-
-        // ASSERT THAT CAF CONTAINS EXPECTED VALUES
-
+    private void assertCafContainsAllFieldsForMinimumSnapFlow(String applicationId) {
         // Page 1
         assertPdfFieldEquals("APPLICATION_ID", applicationId);
         assertPdfFieldEquals("COUNTY_INSTRUCTIONS", "This application was submitted. A caseworker at Hennepin County will help route your application to your county. For more support with your application, you can call Hennepin County at 612-596-1300.");
@@ -170,6 +99,7 @@ public class MinimumSnapFlowJourneyTest extends JourneyTest {
         assertPdfFieldEquals("APPLICANT_HOME_APT_NUMBER", homeApartmentNumber);
         assertPdfFieldEquals("APPLICANT_HOME_CITY", homeCity);
         assertPdfFieldEquals("APPLICANT_HOME_STATE", "MN");
+        assertPdfFieldEquals("APPLICANT_HOME_ZIPCODE", homeZip);
         assertPdfFieldEquals("APPLICANT_MAILING_STREET_ADDRESS", mailingStreetAddress);
         assertPdfFieldEquals("APPLICANT_MAILING_APT_NUMBER", mailingApartmentNumber);
         assertPdfFieldEquals("APPLICANT_MAILING_CITY", mailingCity);
@@ -189,14 +119,96 @@ public class MinimumSnapFlowJourneyTest extends JourneyTest {
         assertPdfFieldEquals("DRUG_FELONY", "No");
         assertPdfFieldEquals("APPLICANT_SIGNATURE", signature);
         assertPdfFieldIsTodayWithFormat("CREATED_DATE", "yyyy-MM-dd");
+    }
 
-        // Assert we actually publish the proper event
-        ArgumentCaptor<ApplicationSubmittedEvent> captor = ArgumentCaptor.forClass(ApplicationSubmittedEvent.class);
-        verify(pageEventPublisher).publish(captor.capture());
-        ApplicationSubmittedEvent applicationSubmittedEvent = captor.getValue();
-        assertThat(applicationSubmittedEvent.getFlow()).isEqualTo(MINIMUM);
-        assertThat(applicationSubmittedEvent.getApplicationId()).isEqualTo(applicationId);
-        assertThat(applicationSubmittedEvent.getLocale()).isEqualTo(ENGLISH);
+    private void getToMinimumFlow() {
+        testPage.clickButton("Apply now");
+        testPage.clickContinue();
+
+        // Language Preferences
+        testPage.enter("writtenLanguage", "English");
+        testPage.enter("spokenLanguage", "English");
+        testPage.enter("needInterpreter", needsInterpreter);
+        testPage.clickContinue();
+
+        // Program Selection
+        testPage.enter("programs", PROGRAM_SNAP);
+        testPage.clickContinue();
+        testPage.clickContinue();
+
+        // Personal Info
+        testPage.enter("firstName", firstName);
+        testPage.enter("lastName", lastName);
+        testPage.enter("otherName", otherName);
+        testPage.enter("dateOfBirth", dateOfBirth);
+        testPage.enter("ssn", "123456789");
+        testPage.enter("maritalStatus", "Never married");
+        testPage.enter("sex", sex);
+        testPage.enter("livedInMnWholeLife", "Yes");
+        testPage.enter("moveToMnDate", moveDate);
+        testPage.enter("moveToMnPreviousCity", previousCity);
+        testPage.clickContinue();
+
+        // How can we get in touch with you?
+        testPage.enter("phoneNumber", "7234567890");
+        testPage.enter("email", email);
+        testPage.enter("phoneOrEmail", "Text me");
+        testPage.clickContinue();
+
+        // Where are you currently Living?
+        testPage.enter("zipCode", homeZip);
+        testPage.enter("city", homeCity);
+        testPage.enter("streetAddress", homeStreetAddress);
+        testPage.enter("apartmentNumber", homeApartmentNumber);
+        testPage.enter("isHomeless", "I don't have a permanent address");
+        testPage.enter("sameMailingAddress", "No, use a different address for mail");
+        testPage.clickContinue();
+        testPage.clickButton("Use this address");
+
+        // Where can the county send your mail? (accept the smarty streets enriched address)
+        testPage.enter("zipCode", "23456");
+        testPage.enter("city", "someCity");
+        testPage.enter("streetAddress", "someStreetAddress");
+        testPage.enter("state", "IL");
+        testPage.enter("apartmentNumber", "someApartmentNumber");
+        when(smartyStreetClient.validateAddress(any())).thenReturn(
+                Optional.of(new Address(mailingStreetAddress, mailingCity, mailingState, mailingZip, mailingApartmentNumber, "someCounty"))
+        );
+        testPage.clickContinue();
+        testPage.clickElementById("enriched-address");
+        testPage.clickContinue();
+
+        // Let's review your info
+        assertThat(driver.findElementById("mailing-address_street").getText()).isEqualTo(mailingStreetAddress);
+
+        testPage.clickLink("Submit application now with only the above information.");
+    }
+
+    private String finishApplicationAndDownloadCaf() {
+        assertThat(testPage.getTitle()).isEqualTo("Additional Info");
+
+        driver.findElement(By.id("additionalInfo")).sendKeys(additionalInfo);
+        testPage.enter("caseNumber", caseNumber);
+        testPage.clickContinue();
+
+        assertThat(testPage.getTitle()).isEqualTo("Legal Stuff");
+        testPage.enter("agreeToTerms", "I agree");
+        testPage.enter("drugFelony", NO.getDisplayValue());
+        testPage.clickContinue();
+        testPage.enter("applicantSignature", signature);
+        testPage.clickButton("Submit");
+
+        // No document upload
+        testPage.clickButton("Skip this for now");
+
+        // Download CAF
+        SuccessPage successPage = new SuccessPage(driver);
+        assertThat(successPage.CAFdownloadPresent()).isTrue();
+        assertThat(successPage.CCAPdownloadPresent()).isFalse();
+        successPage.downloadPdfs();
+        await().until(() -> getAllFiles().size() == successPage.pdfDownloadLinks());
+        caf = getAllFiles().get(CAF);
+        return successPage.getConfirmationNumber(); // Application ID
     }
 
     private void assertPdfFieldEquals(String fieldName, String expectedVal) {
