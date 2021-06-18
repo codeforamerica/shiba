37523418dd8b3343255a6eb0daaaeece34e86add
Alex Gonzalez <spokenbird@gmail.com>
2021-06-17 17:01:30 -0700

    WIP - Implementing Azure Blob Storage
    - Upload working
    
    [#178189751]
    
    Co-authored-by: Luigi Ray-Montanez <luigi@codeforamerica.org>

---
+++ src/main/java/org/codeforamerica/shiba/documents/AzureDocumentRepositoryService.java
@@ -0,0 +1,52 @@
+package org.codeforamerica.shiba.documents;
+
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.blob.models.BlobProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+//@Profile({"demo", "staging", "production"})
+@Slf4j
+public class AzureDocumentRepositoryService implements DocumentRepositoryService {
+    private final String containerName;
+    private final BlobServiceClient blobServiceClient;
+    private final BlobContainerClient containerClient;
+
+    public AzureDocumentRepositoryService() {
+        this.blobServiceClient = new BlobServiceClientBuilder().connectionString(System.getenv("AZURE_CONNECTION_STRING")).buildClient();
+        this.containerClient = blobServiceClient.getBlobContainerClient(System.getenv("AZURE_CONTAINER_NAME"));
+        this.containerName = System.getenv("AZURE_CONTAINER_NAME");
+    }
+
+    @Override
+    public byte[] get(String filepath) {
+
+        BlobClient blobClient = containerClient.getBlobClient(filepath);
+        BlobProperties blobProperties = blobClient.downloadToFile(filepath);
+
+        return blobProperties.getContentMd5();
+    }
+
+    @Override
+    public void upload(String filepath, MultipartFile file) throws IOException, InterruptedException {
+        log.info("Uploading file {} to Azure at filepath {}", file.getOriginalFilename(), filepath);
+        // Get a reference to a blob
+        BlobClient blobClient = containerClient.getBlobClient(filepath);
+        log.info("Uploading to Azure Blob storage as blob:" + blobClient.getBlobUrl());
+        // Upload the blob
+        blobClient.upload(file.getInputStream(), file.getSize());
+        log.info("finished uploading");
+    }
+
+    @Override
+    public void delete(String filepath) {
+
+    }
+}
