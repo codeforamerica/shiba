255203d04c723bd7caa21f714d6466fa326fdcf7
Andrew Edstrom <andrewedstrom@gmail.com>
2021-06-29 10:47:06 -0500

    Remove test that has been replaced
    [#178367955]

--- src/test/java/org/codeforamerica/shiba/pages/ValidationTest.java
+++ src/test/java/org/codeforamerica/shiba/pages/ValidationTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.MessageSource;
 import org.springframework.context.support.StaticMessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
