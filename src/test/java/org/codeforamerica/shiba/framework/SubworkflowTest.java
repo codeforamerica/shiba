81e198c6297b9dd01d65124104eb04c59fdfd5b2
Andrew Edstrom <andrewedstrom@gmail.com>
2021-06-30 09:26:16 -0600

    Convert some subworkflow tests to use mockmvc
    [#178367955]

--- src/test/java/org/codeforamerica/shiba/framework/SubworkflowTest.java
+++ src/test/java/org/codeforamerica/shiba/framework/SubworkflowTest.java
@@ -1,6 +1,7 @@
 package org.codeforamerica.shiba.framework;
 
 import org.codeforamerica.shiba.pages.events.PageEventPublisher;
+import org.codeforamerica.shiba.pages.events.SubworkflowCompletedEvent;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +10,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import java.util.Locale;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest(properties = {"pagesConfig=pages-config/test-sub-workflow.yaml"})
 public class SubworkflowTest extends AbstractFrameworkTest {
@@ -28,15 +31,112 @@ public class SubworkflowTest extends AbstractFrameworkTest {
     @Test
     void shouldDisplayInputFromSubflowInFinalPage() throws Exception {
         postExpectingSuccess("startPage");
+        // skippableFirstPage should be skipped due to skip condition
         assertNavigationRedirectsToCorrectNextPage("startPage", "skippableFirstPage/navigation");
 
         var firstPage = getNextPageAsFormPage("skippableFirstPage");
         assertThat(firstPage.getTitle()).isEqualTo(dummyPageTitle);
 
-        postExpectingSuccessAndAssertRedirectPageNameIsCorrect("firstPage", "input1", "goToSecondPage", "secondPage");
-        postExpectingSuccessAndAssertRedirectPageNameIsCorrect("secondPage", "input2", "text2", "endPage");
+        postExpectingSuccessAndAssertRedirectPageNameIsCorrect("firstPage",
+                                                               "input1",
+                                                               "goToSecondPage",
+                                                               "secondPage");
+        postExpectingSuccessAndAssertRedirectPageNameIsCorrect("secondPage",
+                                                               "input2",
+                                                               "text2",
+                                                               "endPage");
+
+        assertReviewPageDisplaysCorrectInputValueForFirstIteration("goToSecondPage");
+    }
+
+    @Test
+    void shouldSupportSkippableFirstPage() throws Exception {
+        postExpectingSuccessAndAssertRedirectPageNameIsCorrect("startPage",
+                                                               "foo",
+                                                               "someinput",
+                                                               "skippableFirstPage");
+        postExpectingSuccessAndAssertRedirectPageNameIsCorrect("skippableFirstPage",
+                                                               "inputSkippable",
+                                                               "bar",
+                                                               "firstPage");
+        postExpectingSuccessAndAssertRedirectPageNameIsCorrect("firstPage",
+                                                               "input1",
+                                                               "goToSecondPage",
+                                                               "secondPage");
+        postExpectingSuccessAndAssertRedirectPageNameIsCorrect("secondPage",
+                                                               "input2",
+                                                               "text 2",
+                                                               "endPage");
+
+        assertReviewPageDisplaysCorrectInputValueForFirstIteration("goToSecondPage");
+    }
+
+    @Test
+    void shouldSupportSoloPageSubworkflow() throws Exception {
+        postExpectingSuccess("soloPage");
+        verify(pageEventPublisher).publish(any(SubworkflowCompletedEvent.class));
+    }
+
+    @Test
+    void shouldCompleteSubflowInAnyOfTheConfiguredCompletePages() throws Exception {
+        postExpectingSuccess("startPage");
+        assertNavigationRedirectsToCorrectNextPage("startPage", "skippableFirstPage/navigation");
+
+        var firstPage = getNextPageAsFormPage("skippableFirstPage");
+        assertThat(firstPage.getTitle()).isEqualTo(dummyPageTitle);
+
+        postExpectingSuccessAndAssertRedirectPageNameIsCorrect("firstPage",
+                                                               "input1",
+                                                               "goToThirdPage",
+                                                               "thirdPage");
+        postExpectingSuccessAndAssertRedirectPageNameIsCorrect("thirdPage",
+                                                               "input3",
+                                                               "text 3",
+                                                               "endPage");
+        assertReviewPageDisplaysCorrectInputValueForFirstIteration("goToThirdPage");
+        verify(pageEventPublisher).publish(any(SubworkflowCompletedEvent.class));
+    }
+
+    @Test
+    void shouldNotDisplayIterationInEndPageIfIterationWasNotCompleted() throws Exception {
+        // Complete an iteration
+        postExpectingSuccess("startPage");
+        assertNavigationRedirectsToCorrectNextPage("startPage", "skippableFirstPage/navigation");
+        postExpectingSuccessAndAssertRedirectPageNameIsCorrect("skippableFirstPage",
+                                                               "inputSkippable",
+                                                               "bar",
+                                                               "firstPage");
+        postExpectingSuccessAndAssertRedirectPageNameIsCorrect("firstPage",
+                                                               "input1",
+                                                               "goToSecondPage",
+                                                               "secondPage");
+        postExpectingSuccessAndAssertRedirectPageNameIsCorrect("secondPage",
+                                                               "input2",
+                                                               "text 2",
+                                                               "endPage");
+        assertReviewPageDisplaysCorrectInputValueForFirstIteration("goToSecondPage");
+
+
+        // Start another iteration but don't finish it
+        postExpectingSuccessAndAssertRedirectPageNameIsCorrect("firstPage",
+                                                               "input1",
+                                                               "goToThirdPage",
+                                                               "thirdPage");
 
         var endPage = new FormPage(getPage("endPage"));
+        assertThat(endPage.getElementById("iteration0")).isNotNull();
         assertThat(endPage.getElementById("iteration0").text()).isEqualTo("goToSecondPage");
+        assertThat(endPage.getElementById("iteration1")).isNull();
+    }
+
+    /**
+     * endPage is a custom review page which displays the the value that was entered for input1 on firstPage
+     * <p>
+     * This method asserts that the expected input value is shown on the review page
+     */
+    private void assertReviewPageDisplaysCorrectInputValueForFirstIteration(
+            String expectedFirstPageInput1Value) throws Exception {
+        var endPage = new FormPage(getPage("endPage"));
+        assertThat(endPage.getElementById("iteration0").text()).isEqualTo(expectedFirstPageInput1Value);
     }
 }
