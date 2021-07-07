f8e2f3a03e01af6f68865d4903814680e2992513
Alex Gonzalez <spokenbird@gmail.com>
2021-07-07 13:29:24 -0700

    Fix issue with faulty POST
    [#178367953]
    
    Co-authored-by: Andrew Edstrom <aedstrom@codeforamerica.org>

--- src/test/java/org/codeforamerica/shiba/pages/MetricsTest.java
+++ src/test/java/org/codeforamerica/shiba/pages/MetricsTest.java
@@ -16,7 +16,9 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @Tag("ccap")
 @Sql(statements = "TRUNCATE TABLE applications;")
@@ -26,6 +28,10 @@ public class MetricsTest extends AbstractShibaMockMvcTest {
     protected void setUp() throws Exception {
         super.setUp();
         when(featureFlagConfiguration.get("apply-without-address")).thenReturn(FeatureFlag.OFF);
+        when(clock.instant()).thenReturn(
+                LocalDateTime.of(2020, 1, 1, 10, 10).atOffset(ZoneOffset.UTC).toInstant(),
+                LocalDateTime.of(2020, 1, 1, 10, 15, 30).atOffset(ZoneOffset.UTC).toInstant()
+        );
         mockMvc.perform(get("/pages/languagePreferences").session(session)); // start timer
         postExpectingSuccess("languagePreferences", Map.of(
                 "writtenLanguage", List.of("ENGLISH"),
@@ -36,25 +42,23 @@ public class MetricsTest extends AbstractShibaMockMvcTest {
     @Test
     @WithMockUser(username="admin",roles={"USER","ADMIN"})
     void userCanCompleteTheNonExpeditedFlowAndCanDownloadPdfsAndShibaShouldCaptureMetricsAfterApplicationIsCompleted() throws Exception {
-        when(clock.instant()).thenReturn(
-                LocalDateTime.of(2020, 1, 1, 10, 10).atOffset(ZoneOffset.UTC).toInstant(),
-                LocalDateTime.of(2020, 1, 1, 10, 15, 30).atOffset(ZoneOffset.UTC).toInstant()
-        );
         FormPage successPage = nonExpeditedFlowToSuccessPage(false, true);
 
-        assert(successPage.findLinksByText("Combined Application").size() == 1);
-        assert(successPage.findLinksByText("Child Care Application").size() == 1);
-
-        postExpectingSuccess("success", "sentiment", "HAPPY");
+        assertThat(successPage.findLinksByText("Combined Application")).hasSize(1);
+        assertThat(successPage.findLinksByText("Child Care Application")).hasSize(1);
+        mockMvc.perform(post("/submit-feedback")
+                        .session(session)
+                        .with(csrf())
+                        .param("sentiment", "HAPPY"));
 
         FormPage metricsPage = new FormPage(getPageWithAuth("metrics"));
 //
-//        assert(metricsPage.findElementTextById("totals")).contains("Totals");
-//        assertThat(metricsPage.getCardValue("Happy")).contains("100%");
+        assertThat(metricsPage.findElementTextById("totals")).contains("Totals");
+        assertThat(metricsPage.getCardValue("Happy")).contains("100%");
         assertThat(metricsPage.getCardValue("Applications Submitted")).isEqualTo("1");
-//        assertThat(metricsPage.getCardValue("Median All Time")).contains("05m 30s");
-//        assertThat(metricsPage.getCardValue("Median Week to Date")).contains("05m 30s");
-//        assertThat(metricsPage.getCardValue("Average Week to Date")).contains("05m 30s");
+        assertThat(metricsPage.getCardValue("Median All Time")).contains("05m 30s");
+        assertThat(metricsPage.getCardValue("Median Week to Date")).contains("05m 30s");
+        assertThat(metricsPage.getCardValue("Average Week to Date")).contains("05m 30s");
 
         /*
 
