54c738128faae9a5e933b8e473889ffce465c6c4
Andrew Edstrom <andrewedstrom@gmail.com>
2021-07-08 13:10:07 -0700

    Optimize imports
    [#178367953]
    
    Co-authored-by: John Bisek <john.bisek@state.mn.us>

--- src/test/java/org/codeforamerica/shiba/pages/MetricsTest.java
+++ src/test/java/org/codeforamerica/shiba/pages/MetricsTest.java
@@ -1,6 +1,5 @@
 package org.codeforamerica.shiba.pages;
 
-import lombok.extern.slf4j.Slf4j;
 import org.codeforamerica.shiba.AbstractShibaMockMvcTest;
 import org.codeforamerica.shiba.framework.FormPage;
 import org.codeforamerica.shiba.pages.config.FeatureFlag;
