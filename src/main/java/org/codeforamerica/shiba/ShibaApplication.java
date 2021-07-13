ab02bb9a1632a354908b548c69744a0fe97252ba
Andrew Edstrom <andrewedstrom@gmail.com>
2021-07-13 14:54:07 -0700

    Add @EnableScheduling so we can use @Scheduled
    [#178700133]

--- src/main/java/org/codeforamerica/shiba/ShibaApplication.java
+++ src/main/java/org/codeforamerica/shiba/ShibaApplication.java
@@ -3,13 +3,13 @@ package org.codeforamerica.shiba;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ServletComponentScan
+@EnableScheduling
 public class ShibaApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ShibaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ShibaApplication.class, args);
+    }
 }
