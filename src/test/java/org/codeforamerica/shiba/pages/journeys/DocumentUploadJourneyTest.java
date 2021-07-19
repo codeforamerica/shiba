cfd831fdfbc83aa1b1e618dda4529db1fdaba779
Andrew Edstrom <andrewedstrom@gmail.com>
2021-07-19 15:16:25 -0700

    Combine document upload tests
    [#178367957]

--- src/test/java/org/codeforamerica/shiba/pages/journeys/DocumentUploadJourneyTest.java
+++ src/test/java/org/codeforamerica/shiba/pages/journeys/DocumentUploadJourneyTest.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebElement;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
@@ -14,7 +16,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
 
 @Tag("document")
-public class DocumentsTest extends JourneyTest {
+public class DocumentUploadJourneyTest extends JourneyTest {
     @Test
     void whenDocumentUploadFailsThenThereShouldBeAnError() throws InterruptedException {
         doThrow(new InterruptedException())
@@ -31,32 +33,26 @@ public class DocumentsTest extends JourneyTest {
     }
 
     @Test
-    void showMaxFileUploadMessageWhenClientHasUploaded20Documents() {
-        getToDocumentUploadScreen();
-        for (int c = 0; c < 20; c++) {
-            uploadJpgFile();
-        }
-
-        assertThat(driver.findElementById("max-files").getText()).contains("You have uploaded the maximum number of files (20). You will have the opportunity to share more with a county worker later.");
-    }
-
-    @Test
-    void showMaxFilesizeErrorMessageWhenClientHasUploadedLargeDocument() {
-        getToDocumentUploadScreen();
-        long largeFilesize = 21000000L;
-        driver.executeScript("$('#document-upload').get(0).dropzone.addFile({name: 'testFile.pdf', size: " + largeFilesize + ", type: 'not-an-image'})");
-
-        int maxFileSize = uploadDocumentConfiguration.getMaxFilesize();
-        assertThat(driver.findElementByClassName("text--error").getText()).contains("This file is too large and cannot be uploaded (max size: " + maxFileSize + " MB)");
-    }
-
-    @Test
-    void shouldUpdateFileCountWhenRemoveIsClickedIfAnUploadHasAnError() {
+    void shouldShowErrorsAndWarningsProperly() {
         getToDocumentUploadScreen();
         uploadJpgFile();
+
+        // should disallow adding files with types that are not on the allow list
         driver.executeScript("$('#document-upload').get(0).dropzone.addFile({name: 'testFile.xyz', size: 1000, type: 'not-an-image'})");
         assertThat(driver.findElementsByClassName("text--error").get(0).getText()).contains("You can't upload files of this type");
         testPage.clickLink("remove");
         assertThat(driver.findElementById("number-of-uploaded-files").getText()).isEqualTo("1 file added");
+
+        // should show max filesize error message for files that are too big
+        long largeFilesize = 21000000L;
+        driver.executeScript("$('#document-upload').get(0).dropzone.addFile({name: 'testFile.pdf', size: " + largeFilesize + ", type: 'not-an-image'})");
+        int maxFileSize = uploadDocumentConfiguration.getMaxFilesize();
+        assertThat(driver.findElementByClassName("text--error").getText()).contains("This file is too large and cannot be uploaded (max size: " + maxFileSize + " MB)");
+        testPage.clickLink("remove");
+        assertThat(driver.findElementById("number-of-uploaded-files").getText()).isEqualTo("1 file added");
+
+        // should alert the user when they have uploaded the maximum number of files
+        IntStream.range(0, 19).forEach(c -> uploadJpgFile());
+        assertThat(driver.findElementById("max-files").getText()).contains("You have uploaded the maximum number of files (20). You will have the opportunity to share more with a county worker later.");
     }
 }
