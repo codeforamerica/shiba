6c28c31249288cf3baf673aa2a6695d361619181
Andrew Edstrom <andrewedstrom@gmail.com>
2021-07-20 11:48:16 -0700

    add frameworkTest to gradle and CI
    [#178367963]

--- src/test/java/org/codeforamerica/shiba/framework/GoBackButtonTest.java
+++ src/test/java/org/codeforamerica/shiba/framework/GoBackButtonTest.java
@@ -2,6 +2,7 @@ package org.codeforamerica.shiba.framework;
 
 import org.codeforamerica.shiba.AbstractExistingStartTimePageTest;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT, properties = {"pagesConfig=pages-config/test-conditional-rendering.yaml"})
+@Tag("framework")
 public class GoBackButtonTest extends AbstractExistingStartTimePageTest {
     private final String firstPageTitle = "firstPageTitle";
     private final String secondPageTitle = "secondPageTitle";
