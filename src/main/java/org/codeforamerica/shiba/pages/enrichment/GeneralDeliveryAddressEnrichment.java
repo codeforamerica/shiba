c5931afabfed60b0b1455a0358862df884677e9d
Sree P <sprasad@codeforamerica.org>
2021-07-20 11:19:46 -0700

    WIP - Adding city-mapped information for general delivery address TODO - follow-ups and testing
    [#178452089]
    
    Co-authored-by: Eric J <eric.m.johnson@state.mn.us>

---
+++ src/main/java/org/codeforamerica/shiba/pages/enrichment/GeneralDeliveryAddressEnrichment.java
@@ -0,0 +1,43 @@
+package org.codeforamerica.shiba.pages.enrichment;
+
+import org.codeforamerica.shiba.County;
+import org.codeforamerica.shiba.CountyMap;
+import org.codeforamerica.shiba.configurations.CityInfoConfiguration;
+import org.codeforamerica.shiba.mnit.MnitCountyInformation;
+import org.codeforamerica.shiba.pages.data.InputData;
+import org.codeforamerica.shiba.pages.data.PagesData;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class GeneralDeliveryAddressEnrichment implements Enrichment {
+    private final CityInfoConfiguration cityInfoConfiguration;
+    private final CountyMap<MnitCountyInformation> countyMap;
+
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    public GeneralDeliveryAddressEnrichment(CityInfoConfiguration cityInfoConfiguration, CountyMap<MnitCountyInformation> countyMap) {
+        this.cityInfoConfiguration = cityInfoConfiguration;
+        this.countyMap = countyMap;
+    }
+
+    @Override
+    public EnrichmentResult process(PagesData pagesData) {
+        String cityname = pagesData.getPageInputFirstValue("cityForGeneralDelivery", "whatIsTheCity");
+//        String cityName = getFirstValue(pagesData, GENERAL_DELIVERY_CITY); TODO replace when available
+
+        Map<String, String> cityInfo = cityInfoConfiguration.getCityToZipAndCountyMapping().get(cityname);
+
+        String countyFromCity = cityInfo.get("county");
+        String zipcodeFromCity = cityInfo.get("zipcode");
+        County county = County.valueFor(countyFromCity);
+        String phoneNumber = countyMap.get(county).getPhoneNumber();
+        String displayCounty = county == County.Other ? County.Other.displayName() : county.displayName() + " County";
+
+        return new EnrichmentResult(Map.of(
+                "enrichedPhoneNumber", new InputData(List.of(phoneNumber)),
+                "enrichedZipcode", new InputData(List.of(zipcodeFromCity))
+        ));
+    }
+}
