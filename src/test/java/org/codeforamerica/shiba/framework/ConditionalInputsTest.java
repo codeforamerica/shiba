677e7d945b16115b2abd894273ec42f1de74523f
Andrew Edstrom <andrewedstrom@gmail.com>
2021-07-22 17:18:36 -0700

    Convert ConditionalInputsPageTest to MockMvc
    [#178368011]

---
+++ src/test/java/org/codeforamerica/shiba/framework/ConditionalInputsTest.java
@@ -0,0 +1,32 @@
+package org.codeforamerica.shiba.framework;
+
+import org.codeforamerica.shiba.testutilities.AbstractFrameworkTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static java.util.Locale.ENGLISH;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(properties = {"pagesConfig=pages-config/test-conditional-inputs.yaml"})
+public class ConditionalInputsTest extends AbstractFrameworkTest {
+
+    @Override
+    @BeforeEach
+    protected void setUp() throws Exception {
+        super.setUp();
+        staticMessageSource.addMessage("option1", ENGLISH, "option 1");
+        staticMessageSource.addMessage("option2", ENGLISH, "option 2");
+        staticMessageSource.addMessage("option3", ENGLISH, "option 3");
+    }
+
+    @Test
+    void shouldOnlyRenderInputsBasedOnCondition() throws Exception {
+        var page = postAndFollowRedirect("firstPage", "options", List.of("option1", "option2"));
+        assertThat(page.findInputByName("option1Text")).isNotNull();
+        assertThat(page.findInputByName("option2Text")).isNotNull();
+        assertThat(page.findInputByName("option3Text")).isNull();
+    }
+}
