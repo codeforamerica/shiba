f234d575f54e6843bc993e72561a46c918ad84e6
Sree <72890349+sree-cfa@users.noreply.github.com>
2021-07-23 13:50:41 -0700

    Replacing mailing derived-values with an applicationInputMapper class
    - derived-values do not currently support feature flags or nested conditions 
    
    [#178458259]
    
    Co-authored-by: Eric J <eric.m.johnson@state.mn.us>

--- src/main/java/org/codeforamerica/shiba/output/caf/HomeAddressStreetMapper.java
+++ src/main/java/org/codeforamerica/shiba/output/caf/HomeAddressStreetMapper.java
@@ -7,36 +7,50 @@ import org.codeforamerica.shiba.output.Document;
 import org.codeforamerica.shiba.output.Recipient;
 import org.codeforamerica.shiba.output.applicationinputsmappers.ApplicationInputsMapper;
 import org.codeforamerica.shiba.output.applicationinputsmappers.SubworkflowIterationScopeTracker;
+import org.codeforamerica.shiba.pages.config.FeatureFlagConfiguration;
 import org.codeforamerica.shiba.pages.data.PageData;
+import org.codeforamerica.shiba.pages.data.PagesData;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 public class HomeAddressStreetMapper implements ApplicationInputsMapper {
+    private final FeatureFlagConfiguration featureFlagConfiguration;
+    private final static String NO_PERMANENT_ADDRESS = "No permanent address";
+
+    public HomeAddressStreetMapper(FeatureFlagConfiguration featureFlagConfiguration) {
+        this.featureFlagConfiguration = featureFlagConfiguration;
+    }
+
     @Override
     public List<ApplicationInput> map(Application application, Document document, Recipient recipient, SubworkflowIterationScopeTracker scopeTracker) {
-        PageData homeAddressPageData = application.getApplicationData().getPagesData().getPage("homeAddress");
+        PagesData pagesData = application.getApplicationData().getPagesData();
+        PageData homeAddressPageData = pagesData.getPage("homeAddress");
         if (homeAddressPageData == null) {
             return List.of();
         }
+
         if (String.join("", homeAddressPageData.get("streetAddress").getValue()).isBlank()) {
             return List.of(new ApplicationInput(
                     "homeAddress",
                     "streetAddressWithPermanentAddress",
-                    List.of("No permanent address"),
+                    List.of(NO_PERMANENT_ADDRESS),
                     ApplicationInputType.SINGLE_VALUE
             ));
         }
 
-        String notPermanentAddressIndicator = homeAddressPageData.get("isHomeless").getValue().isEmpty() ?
-                "" : " (not permanent)";
-        String usesEnriched = application.getApplicationData().getPagesData()
-                .getPage("homeAddressValidation")
-                .get("useEnrichedAddress")
-                .getValue().get(0);
+        String usesEnriched = pagesData.getPageInputFirstValue("homeAddressValidation", "useEnrichedAddress");
         String streetInputName = Boolean.parseBoolean(usesEnriched) ? "enrichedStreetAddress" : "streetAddress";
-        String value = homeAddressPageData.get(streetInputName).getValue().get(0) + notPermanentAddressIndicator;
+
+        String value = homeAddressPageData.get(streetInputName).getValue().get(0);
+
+        if (featureFlagConfiguration.get("apply-without-address").isOff()) {
+            boolean isHomeless = !homeAddressPageData.get("isHomeless").getValue().isEmpty();
+            String notPermanentAddressIndicator = isHomeless ? " (not permanent)" : "";
+            value += notPermanentAddressIndicator;
+        }
+
         return List.of(
                 new ApplicationInput(
                         "homeAddress",
