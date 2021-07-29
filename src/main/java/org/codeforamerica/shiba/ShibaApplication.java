c1a9ad8a26b4c87f3224c782b45aeca02e314832
britneyepps <bepps@codeforamerica.org>
2021-07-29 12:17:49 -0400

    Adding in library for shedlock, added @EnableSchedulerLock
    [#179048278]

--- src/main/java/org/codeforamerica/shiba/ShibaApplication.java
+++ src/main/java/org/codeforamerica/shiba/ShibaApplication.java
@@ -1,5 +1,6 @@
 package org.codeforamerica.shiba;
 
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -8,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @ServletComponentScan
 @EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "12h")
 public class ShibaApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShibaApplication.class, args);
