b3332d4a0896709096de79a352be88fdff77df27
Andrew Edstrom <andrewedstrom@gmail.com>
2021-08-16 17:12:19 -0700

    Formatting for shortened success page
    [#179093870]

--- src/test/java/org/codeforamerica/shiba/testutilities/SuccessPage.java
+++ src/test/java/org/codeforamerica/shiba/testutilities/SuccessPage.java
@@ -1,7 +1,5 @@
 package org.codeforamerica.shiba.testutilities;
 
-import org.codeforamerica.shiba.pages.Sentiment;
-import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -9,56 +7,53 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class SuccessPage extends Page {
-    @FindBy(linkText = "Combined Application")
-    WebElement downloadCafLink;
 
-    @FindBy(linkText = "Child Care Application")
-    WebElement downloadCCAPApplicationLink;
+  @FindBy(id = "download-caf")
+  private WebElement downloadCafLink;
 
-    @FindBy(id = "application-id")
-    WebElement confirmationNumber;
+  @FindBy(id = "download-ccap")
+  private WebElement downloadCCAPApplicationLink;
 
-    public SuccessPage(RemoteWebDriver driver) {
-        super(driver);
-        PageFactory.initElements(driver, this);
+  @FindBy(id = "application-id")
+  private WebElement confirmationNumber;
+
+  public SuccessPage(RemoteWebDriver driver) {
+    super(driver);
+    PageFactory.initElements(driver, this);
+  }
+
+  public void downloadPdfs() {
+    if (CAFdownloadPresent()) {
+      downloadCafLink.click();
     }
-
-    public void downloadPdfs() {
-        if (CAFdownloadPresent()) downloadCafLink.click();
-        if (CCAPdownloadPresent()) downloadCCAPApplicationLink.click();
+    if (CCAPdownloadPresent()) {
+      downloadCCAPApplicationLink.click();
     }
+  }
 
-    public boolean CAFdownloadPresent() {
-        try {
-            return downloadCafLink.isDisplayed();
-        } catch (NoSuchElementException e) {
-            return false;
-        }
+  public boolean CAFdownloadPresent() {
+    try {
+      return downloadCafLink.isDisplayed();
+    } catch (NoSuchElementException e) {
+      return false;
     }
+  }
 
-    public boolean CCAPdownloadPresent() {
-        try {
-            return downloadCCAPApplicationLink.isDisplayed();
-        } catch (NoSuchElementException e) {
-            return false;
-        }
+  public boolean CCAPdownloadPresent() {
+    try {
+      return downloadCCAPApplicationLink.isDisplayed();
+    } catch (NoSuchElementException e) {
+      return false;
     }
+  }
 
-    public String getConfirmationNumber() {
-        confirmationNumber.getText(); // "Confirmation #: 3830000507"
-        String[] confirmationNumParts = confirmationNumber.getText().split(" "); // ["Confirmation", "#:", "3830000507"]
-        return confirmationNumParts[confirmationNumParts.length - 1];
-    }
+  public String getConfirmationNumber() {
+    String[] confirmationNumParts =
+        confirmationNumber.getText().split(" "); // ["Confirmation", "#:", "3830000507"]
+    return confirmationNumParts[confirmationNumParts.length - 1];
+  }
 
-    public void chooseSentiment(Sentiment sentiment) {
-        driver.findElement(By.cssSelector(String.format("label[for='%s']", sentiment.name().toLowerCase()))).click();
-    }
-
-    public void submitFeedback() {
-        driver.findElement(By.cssSelector("button")).click();
-    }
-
-    public int pdfDownloadLinks() {
-        return driver.findElementsByClassName("button--link").size();
-    }
+  public int pdfDownloadLinks() {
+    return driver.findElementsByClassName("button--link").size();
+  }
 }
