57ba822313fed90eff6cbe90d2678363fe10745c
Ben Calegari <bcalegari@codeforamerica.org>
2021-08-30 22:25:04 -0400

    Switch ByteArrayDataSource from javax to Apache Commons Email library

--- src/main/java/org/codeforamerica/shiba/mnit/MnitEsbWebServiceClient.java
+++ src/main/java/org/codeforamerica/shiba/mnit/MnitEsbWebServiceClient.java
@@ -7,7 +7,6 @@ import static org.codeforamerica.shiba.output.Document.CAF;
 import static org.codeforamerica.shiba.output.Document.CCAP;
 import static org.codeforamerica.shiba.output.Document.UPLOADED_DOC;
 
-import com.sun.istack.ByteArrayDataSource;
 import com.sun.xml.messaging.saaj.soap.name.NameImpl;
 import java.math.BigInteger;
 import java.time.Clock;
@@ -15,6 +14,7 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import javax.activation.DataHandler;
+import javax.mail.util.ByteArrayDataSource;
 import javax.xml.namespace.QName;
 import javax.xml.soap.SOAPElement;
 import javax.xml.soap.SOAPException;
