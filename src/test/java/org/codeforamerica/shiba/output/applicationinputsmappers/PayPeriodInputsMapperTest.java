997e9e904a565925fd7d05a9db634dc45f7e0555
Sree P <sprasad@codeforamerica.org>
2021-09-02 17:04:55 -0700

    Converting paidByTheHour from derived value to input mapper.
    [#179381880]

---
+++ src/test/java/org/codeforamerica/shiba/output/applicationinputsmappers/PayPeriodInputsMapperTest.java
@@ -0,0 +1,52 @@
+package org.codeforamerica.shiba.output.applicationinputsmappers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Map;
+import org.codeforamerica.shiba.application.Application;
+import org.codeforamerica.shiba.output.ApplicationInput;
+import org.codeforamerica.shiba.output.ApplicationInputType;
+import org.codeforamerica.shiba.pages.data.ApplicationData;
+import org.codeforamerica.shiba.pages.data.Subworkflow;
+import org.codeforamerica.shiba.pages.data.Subworkflows;
+import org.codeforamerica.shiba.testutilities.PageDataBuilder;
+import org.codeforamerica.shiba.testutilities.PagesDataBuilder;
+import org.junit.jupiter.api.Test;
+
+public class PayPeriodInputsMapperTest {
+
+  private final PayPeriodInputsMapper mapper = new PayPeriodInputsMapper();
+
+  @Test
+  public void shouldCreateInputsForHourlyOnly() {
+    PagesDataBuilder pagesDataBuilder = new PagesDataBuilder();
+    ApplicationData applicationData = new ApplicationData();
+    applicationData.setSubworkflows(
+        new Subworkflows(Map.of("jobs", new Subworkflow(List.of(
+            pagesDataBuilder.build(List.of(
+                new PageDataBuilder("paidByTheHour", Map.of("paidByTheHour", List.of("false"))),
+                new PageDataBuilder("payPeriod", Map.of("payPeriod", List.of("EVERY_WEEK"))),
+                new PageDataBuilder("incomePerPayPeriod",
+                    Map.of("incomePerPayPeriod", List.of("1.1")))
+            )),
+            pagesDataBuilder.build(List.of(
+                new PageDataBuilder("paidByTheHour", Map.of("paidByTheHour", List.of("true"))),
+                new PageDataBuilder("incomePerPayPeriod",
+                    Map.of("incomePerPayPeriod", List.of("1.1")))
+            ))
+        )))));
+    List<ApplicationInput> result = mapper.map(Application.builder()
+        .applicationData(applicationData)
+        .build(), null, null, null);
+
+    assertThat(result).containsOnly(
+        new ApplicationInput(
+            "payPeriod",
+            "payPeriod",
+            List.of("Hourly"),
+            ApplicationInputType.SINGLE_VALUE,
+            1
+        ));
+  }
+}
