f686420aec89aeec6d3a7902247a4382d0d9d3b4
Andrew Edstrom <andrewedstrom@gmail.com>
2021-09-13 16:55:27 -0700

    Clean up NextStepsContentService
    [#179415712]

--- src/main/java/org/codeforamerica/shiba/pages/NextStepsContentService.java
+++ src/main/java/org/codeforamerica/shiba/pages/NextStepsContentService.java
@@ -5,11 +5,11 @@ import static org.codeforamerica.shiba.Program.CCAP;
 import static org.codeforamerica.shiba.Program.EA;
 import static org.codeforamerica.shiba.Program.GRH;
 import static org.codeforamerica.shiba.Program.SNAP;
+import static org.codeforamerica.shiba.internationalization.InternationalizationUtils.listToString;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import org.codeforamerica.shiba.internationalization.InternationalizationUtils;
 import org.codeforamerica.shiba.internationalization.LocaleSpecificMessageSource;
 import org.codeforamerica.shiba.output.caf.CcapExpeditedEligibility;
 import org.codeforamerica.shiba.output.caf.SnapExpeditedEligibility;
@@ -18,6 +18,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class NextStepsContentService {
+
+  private final String PHONE_ICON = "fragments/icons/icon-phone :: icon-phone";
+  private final String LETTER_ICON = "fragments/icons/icon-letter :: icon-letter";
+  private final String COMMUNICATE_ICON = "fragments/icons/icon-communicate :: icon-communicate";
 
   private final MessageSource messageSource;
 
@@ -37,36 +41,28 @@ public class NextStepsContentService {
 
     // Expedited Snap timing
     if (isSnapExpeditedEligible) {
-      messages.add(
-          new SuccessMessage("fragments/icons/icon-phone :: icon-phone",
-              lms.getMessage("success.expedited-snap-timing"),
-              lms.getMessage("success.expedited-snap-timing-header"))
+      messages.add(new SuccessMessage(PHONE_ICON,
+          lms.getMessage("success.expedited-snap-timing"),
+          lms.getMessage("success.expedited-snap-timing-header"))
       );
     }
 
     // Expedited Ccap timing
     if (isCcapExpeditedEligible) {
-      messages.add(
-          new SuccessMessage(
-              "fragments/icons/icon-letter :: icon-letter",
-              lms.getMessage("success.expedited-ccap-timing"),
-              lms.getMessage("success.expedited-ccap-timing-header"))
+      messages.add(new SuccessMessage(LETTER_ICON,
+          lms.getMessage("success.expedited-ccap-timing"),
+          lms.getMessage("success.expedited-ccap-timing-header"))
       );
     }
 
-    // Contact Promise
-    List<String> nextStepLetterPrograms = getNextStepLetterPrograms(programs,
-        isSnapExpeditedEligible, isCcapExpeditedEligible, lms);
-    if (!nextStepLetterPrograms.isEmpty()) {
-      String programsInNextStepLetter =
-          InternationalizationUtils.listToString(nextStepLetterPrograms, lms);
-      String contactPromise = lms.getMessage("success.contact-promise",
-          List.of(programsInNextStepLetter));
-      String header = lms.getMessage("success.contact-promise-header");
-      messages.add(new SuccessMessage(
-          "fragments/icons/icon-letter :: icon-letter",
-          contactPromise,
-          header));
+    // Contact Promise for all programs they are not expedited for
+    List<String> nonExpeditedPrograms =
+        getNonExpeditedPrograms(programs, isSnapExpeditedEligible, isCcapExpeditedEligible, lms);
+    if (!nonExpeditedPrograms.isEmpty()) {
+      String humanReadableProgramList = listToString(nonExpeditedPrograms, lms);
+      messages.add(new SuccessMessage(LETTER_ICON,
+          lms.getMessage("success.contact-promise", List.of(humanReadableProgramList)),
+          lms.getMessage("success.contact-promise-header")));
     }
 
     // Suggested Action
@@ -74,37 +70,38 @@ public class NextStepsContentService {
     if (isSnapExpeditedEligible && !programs.contains(CCAP)) {
       suggestedAction = lms.getMessage("success.expedited-snap-suggested-action");
     }
-    messages.add(new SuccessMessage("fragments/icons/icon-communicate :: icon-communicate",
+    messages.add(new SuccessMessage(COMMUNICATE_ICON,
         suggestedAction,
         lms.getMessage("success.suggested-action-header")));
 
     return messages;
   }
 
-  private List<String> getNextStepLetterPrograms(List<String> allPrograms,
+  private List<String> getNonExpeditedPrograms(
+      List<String> programAcronyms,
       boolean isSnapExpeditedEligible,
       boolean isCcapExpeditedEligible,
       LocaleSpecificMessageSource ms) {
     List<String> nextStepLetterPrograms = new ArrayList<>();
 
-    boolean hasNonExpeditedCcap = allPrograms.contains(CCAP) && !isCcapExpeditedEligible;
+    boolean hasNonExpeditedCcap = programAcronyms.contains(CCAP) && !isCcapExpeditedEligible;
     if (hasNonExpeditedCcap) {
       nextStepLetterPrograms.add(ms.getMessage("success.childcare"));
     }
 
-    if (allPrograms.contains(GRH)) {
+    if (programAcronyms.contains(GRH)) {
       nextStepLetterPrograms.add(ms.getMessage("success.housing"));
     }
 
-    if (allPrograms.contains(EA)) {
+    if (programAcronyms.contains(EA)) {
       nextStepLetterPrograms.add(ms.getMessage("success.emergency-assistance"));
     }
 
-    if (allPrograms.contains(CASH)) {
+    if (programAcronyms.contains(CASH)) {
       nextStepLetterPrograms.add(ms.getMessage("success.cash-support"));
     }
 
-    boolean hasNonExpeditedSnap = allPrograms.contains(SNAP) && !isSnapExpeditedEligible;
+    boolean hasNonExpeditedSnap = programAcronyms.contains(SNAP) && !isSnapExpeditedEligible;
     if (hasNonExpeditedSnap) {
       nextStepLetterPrograms.add(ms.getMessage("success.food-support"));
     }
