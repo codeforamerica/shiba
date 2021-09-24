f0e474fb92f758c0eb575e077a817e072638623c
Sree <72890349+sree-cfa@users.noreply.github.com>
2021-09-24 10:46:50 -0700

    Refactoring follow-up of [#179381846] (#441)
    Co-authored-by: Andrew Edstrom <aedstrom@codeforamerica.org>

---
+++ src/main/java/org/codeforamerica/shiba/output/applicationinputsmappers/UnearnedIncomeCcapInputsMapper.java
@@ -0,0 +1,22 @@
+package org.codeforamerica.shiba.output.applicationinputsmappers;
+
+import static org.codeforamerica.shiba.application.parsers.ApplicationDataParser.Field.UNEARNED_INCOME_CCAP;
+
+import java.util.List;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UnearnedIncomeCcapInputsMapper extends OneToManyApplicationInputsMapper {
+
+  private static final List<String> UNEARNED_INCOME_CCAP_OPTIONS = List.of("BENEFITS",
+      "INSURANCE_PAYMENTS", "CONTRACT_FOR_DEED", "TRUST_MONEY", "HEALTH_CARE_REIMBURSEMENT",
+      "INTEREST_DIVIDENDS", "OTHER_SOURCES");
+
+  @Override
+  protected OneToManyParams getParams() {
+    return new OneToManyParams(
+        "unearnedIncomeCcap",
+        UNEARNED_INCOME_CCAP,
+        UNEARNED_INCOME_CCAP_OPTIONS);
+  }
+}
---
+++ src/test/java/org/codeforamerica/shiba/output/applicationinputsmappers/HomeExpensesInputsMapperTest.java
@@ -0,0 +1,68 @@
+package org.codeforamerica.shiba.output.applicationinputsmappers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.codeforamerica.shiba.testutilities.TestUtils.createApplicationInput;
+
+import java.util.List;
+import org.codeforamerica.shiba.application.Application;
+import org.codeforamerica.shiba.output.ApplicationInput;
+import org.codeforamerica.shiba.pages.data.ApplicationData;
+import org.codeforamerica.shiba.testutilities.TestApplicationDataBuilder;
+import org.junit.jupiter.api.Test;
+
+public class HomeExpensesInputsMapperTest {
+
+  private final HomeExpensesInputsMapper mapper = new HomeExpensesInputsMapper();
+
+  @Test
+  public void shouldMapNoneOfTheAboveToNoForAllOptions() {
+    ApplicationData applicationData = new TestApplicationDataBuilder()
+        .withPageData("homeExpenses", "homeExpenses", List.of("NONE_OF_THE_ABOVE"))
+        .build();
+
+    List<ApplicationInput> result = mapper.map(Application.builder()
+        .applicationData(applicationData)
+        .build(), null, null, null);
+
+    assertThat(result).containsOnly(
+        createApplicationInput("homeExpenses", "RENT", "false"),
+        createApplicationInput("homeExpenses", "MORTGAGE", "false"),
+        createApplicationInput("homeExpenses", "HOMEOWNERS_INSURANCE", "false"),
+        createApplicationInput("homeExpenses", "REAL_ESTATE_TAXES", "false"),
+        createApplicationInput("homeExpenses", "ASSOCIATION_FEES", "false"),
+        createApplicationInput("homeExpenses", "ROOM_AND_BOARD", "false")
+    );
+  }
+
+  @Test
+  public void shouldMapYesToSelectedOptions() {
+    ApplicationData applicationData = new TestApplicationDataBuilder()
+        .withPageData("homeExpenses", "homeExpenses",
+            List.of("REAL_ESTATE_TAXES", "ASSOCIATION_FEES"))
+        .build();
+
+    List<ApplicationInput> result = mapper.map(Application.builder()
+        .applicationData(applicationData)
+        .build(), null, null, null);
+
+    assertThat(result).containsOnly(
+        createApplicationInput("homeExpenses", "RENT", "false"),
+        createApplicationInput("homeExpenses", "MORTGAGE", "false"),
+        createApplicationInput("homeExpenses", "HOMEOWNERS_INSURANCE", "false"),
+        createApplicationInput("homeExpenses", "REAL_ESTATE_TAXES", "true"),
+        createApplicationInput("homeExpenses", "ASSOCIATION_FEES", "true"),
+        createApplicationInput("homeExpenses", "ROOM_AND_BOARD", "false")
+    );
+  }
+
+  @Test
+  public void shouldReturnEmptyForMissingData() {
+    ApplicationData applicationData = new ApplicationData();
+    List<ApplicationInput> result = mapper.map(Application.builder()
+        .applicationData(applicationData)
+        .build(), null, null, null);
+
+    assertThat(result).isEmpty();
+  }
+  
+}
