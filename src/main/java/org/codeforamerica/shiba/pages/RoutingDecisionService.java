cf6d3029b51da9cef9e330156c5bb82194628000
Andrew Edstrom <andrewedstrom@gmail.com>
2021-09-29 16:04:05 -0700

    stop multi returning

--- src/main/java/org/codeforamerica/shiba/pages/RoutingDecisionService.java
+++ src/main/java/org/codeforamerica/shiba/pages/RoutingDecisionService.java
@@ -43,36 +43,37 @@ public class RoutingDecisionService {
   // GOAL: this method needs to return white earth
   public List<RoutingDestination> getRoutingDestinations(ApplicationData applicationData,
       Document document) {
-    Set<String> programs = applicationData.getApplicantAndHouseholdMemberPrograms(); // ["CCAP", "SNAP", "EA"]
+    Set<String> programs = applicationData.getApplicantAndHouseholdMemberPrograms();
     County county = countyParser.parse(applicationData);
-    List<RoutingDestination> result = new ArrayList<>(); // []
+    List<RoutingDestination> result = new ArrayList<>();
 
-    if (shouldSendToWhiteEarth(applicationData, document, county)) {
+    boolean shouldSendToWhiteEarth = shouldSendToWhiteEarth(applicationData, county);
+    if (shouldSendToWhiteEarth) {
       result.add(tribalNations.get(WHITE_EARTH));
-      return result;
     }
 
-    boolean shouldSendToMilleLacs = shouldSendToMilleLacs(applicationData,
-        document); // need to change this
+    boolean shouldSendToMilleLacs = shouldSendToMilleLacs(applicationData, document, county);
     if (shouldSendToMilleLacs) {
-      result.add(tribalNations.get(MILLE_LACS_BAND_OF_OJIBWE)); // ["Mille Lacs Band of Ojibwe"]
+      result.add(tribalNations.get(MILLE_LACS_BAND_OF_OJIBWE));
     }
 
-    // Send to county for all other programs
-    if (!shouldSendToMilleLacs
-        || programs.contains(SNAP) || programs.contains(CASH)
-        || programs.contains(GRH) || programs.contains(CCAP)) { // need to change this
-
+    if (shouldSendToCounty(programs, applicationData, document, county)) {
       result.add(countyRoutingDestinations.get(county));
     }
 
-    return result; // getting this: ["Mille Lacs Band of Ojibwe", "Becker"]
-    // we want this: ["White Earth"]
+    return result;
   }
 
-  private boolean shouldSendToWhiteEarth(ApplicationData applicationData, Document document,
-      County county) {
-    // What tribe are they in?
+  private boolean shouldSendToCounty(Set<String> programs,
+      ApplicationData applicationData, Document document, County county) {
+    boolean shouldSendToWhiteEarth = shouldSendToWhiteEarth(applicationData, county);
+    boolean shouldSendToMilleLacs = shouldSendToMilleLacs(applicationData, document, county);
+    return !shouldSendToWhiteEarth && (!shouldSendToMilleLacs
+        || programs.contains(SNAP) || programs.contains(CASH)
+        || programs.contains(GRH) || programs.contains(CCAP));
+  }
+
+  private boolean shouldSendToWhiteEarth(ApplicationData applicationData, County county) {
     var pagesData = applicationData.getPagesData();
     var selectedTribeName = getFirstValue(pagesData, SELECTED_TRIBAL_NATION);
 
@@ -82,7 +83,8 @@ public class RoutingDecisionService {
   }
 
   // Send to Mille Lacs if the tribe is serviced by Mille Lacs and applying for Tribal TANF and/or EA
-  private boolean shouldSendToMilleLacs(ApplicationData applicationData, Document document) {
+  private boolean shouldSendToMilleLacs(ApplicationData applicationData, Document document,
+      County county) {
     var pagesData = applicationData.getPagesData();
     var selectedTribeName = getFirstValue(pagesData, SELECTED_TRIBAL_NATION);
     var applyingForTribalTanf = Boolean.parseBoolean(
@@ -93,6 +95,7 @@ public class RoutingDecisionService {
         && tribalNations.get(selectedTribeName) != null
         && tribalNations.get(selectedTribeName).isServicedByMilleLacs()
         && (applyingForTribalTanf || programs.contains(EA))
-        && !Document.CCAP.equals(document);
+        && !Document.CCAP.equals(document)
+        && !shouldSendToWhiteEarth(applicationData, county);
   }
 }
