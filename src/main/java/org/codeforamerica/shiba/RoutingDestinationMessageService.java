1fbeb0acbcbbd043297efd43f6c0a463ff968ce5
Alex Gonzalez <spokenbird@gmail.com>
2021-10-05 15:03:54 -0700

    Move logic to method

--- src/main/java/org/codeforamerica/shiba/RoutingDestinationMessageService.java
+++ src/main/java/org/codeforamerica/shiba/RoutingDestinationMessageService.java
@@ -14,38 +14,41 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class RoutingDestinationMessageService {
+
   private final MessageSource messageSource;
 
   public RoutingDestinationMessageService(MessageSource messageSource) {
     this.messageSource = messageSource;
   }
 
-  public List<String> generateCcapMessageStrings(Locale locale, County county, List<RoutingDestination> routingDestinations) {
+  public List<String> generateCcapMessageStrings(Locale locale, County county,
+      List<RoutingDestination> routingDestinations) {
     return List.of(generatePhraseWithCountyOnly(locale, county, false, routingDestinations),
         generatePhraseWithCountyOnly(locale, county, true, routingDestinations));
   }
 
-  public List<String> generateCafMessageStrings(Locale locale, County county, List<RoutingDestination> routingDestinations) {
-    return List.of(generatePhrase(locale, county, false, routingDestinations), generatePhrase(locale, county, true, routingDestinations));
+  public List<String> generateCafMessageStrings(Locale locale, County county,
+      List<RoutingDestination> routingDestinations) {
+    return List.of(generatePhrase(locale, county, false, routingDestinations),
+        generatePhrase(locale, county, true, routingDestinations));
   }
 
-  public String generateSuccessPageMessageString(Locale locale, County county, List<RoutingDestination> routingDestinations) {
+  public String generateSuccessPageMessageString(Locale locale, County county,
+      List<RoutingDestination> routingDestinations) {
     return generatePhrase(locale, county, true, routingDestinations);
   }
 
-  private String generatePhraseWithCountyOnly(Locale locale, County county, boolean withPhoneNumbers,
+  private String generatePhraseWithCountyOnly(Locale locale, County county,
+      boolean withPhoneNumbers,
       List<RoutingDestination> routingDestinations) {
     LocaleSpecificMessageSource lms = new LocaleSpecificMessageSource(locale, messageSource);
     List<String> routingDestinationStrings = routingDestinations.stream()
         .filter(rd -> rd instanceof CountyRoutingDestination).map(rd -> {
-      String clientCounty = rd.getName();
-      if (county == County.Other) {
-        clientCounty = County.Hennepin.displayName();
-      }
-      return withPhoneNumbers ? lms.getMessage("general.county-and-phone", List.of(clientCounty, rd.getPhoneNumber())) :
-          lms.getMessage("general.county", List.of(clientCounty));
-
-    })
+          String clientCounty = setCountyName(county, rd);
+          return withPhoneNumbers ? lms
+              .getMessage("general.county-and-phone", List.of(clientCounty, rd.getPhoneNumber())) :
+              lms.getMessage("general.county", List.of(clientCounty));
+        })
         .collect(Collectors.toList());
     return listToString(routingDestinationStrings, lms);
   }
@@ -54,19 +57,26 @@ public class RoutingDestinationMessageService {
       List<RoutingDestination> routingDestinations) {
     LocaleSpecificMessageSource lms = new LocaleSpecificMessageSource(locale, messageSource);
     List<String> routingDestinationStrings = routingDestinations.stream().map(rd -> {
-          if (rd instanceof TribalNationRoutingDestination) {
-            return withPhoneNumbers ? rd.getName() + " Tribal Nation Servicing Agency " + rd.getPhoneNumber():
-                rd.getName() + " Tribal Nation Servicing Agency";
-          }
-          String clientCounty = rd.getName();
-          if (county == County.Other) {
-            clientCounty = County.Hennepin.displayName();
-          }
-          return withPhoneNumbers ? lms.getMessage("general.county-and-phone", List.of(clientCounty, rd.getPhoneNumber())) :
-              lms.getMessage("general.county", List.of(clientCounty));
+      if (rd instanceof TribalNationRoutingDestination) {
+        return withPhoneNumbers ? rd.getName() + " Tribal Nation Servicing Agency " + rd
+            .getPhoneNumber() :
+            rd.getName() + " Tribal Nation Servicing Agency";
+      }
+      String clientCounty = setCountyName(county, rd);
+      return withPhoneNumbers ? lms
+          .getMessage("general.county-and-phone", List.of(clientCounty, rd.getPhoneNumber())) :
+          lms.getMessage("general.county", List.of(clientCounty));
 
-        })
+    })
         .collect(Collectors.toList());
     return listToString(routingDestinationStrings, lms);
+  }
+
+  private String setCountyName(County county, RoutingDestination routingDestination) {
+    String clientCounty = routingDestination.getName();
+    if (county == County.Other) {
+      clientCounty = County.Hennepin.displayName();
+    }
+    return clientCounty;
   }
 }
