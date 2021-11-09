f772869b19e2126f699320eb3eb42f86666698eb
Andrew Edstrom <andrewedstrom@gmail.com>
2021-11-08 16:49:46 -0800

    remove flatmap from SubworkflowIterationScopeTracker
    [#180239973]

--- src/main/java/org/codeforamerica/shiba/output/documentfieldpreparers/SubworkflowIterationScopeTracker.java
+++ src/main/java/org/codeforamerica/shiba/output/documentfieldpreparers/SubworkflowIterationScopeTracker.java
@@ -1,12 +1,7 @@
 package org.codeforamerica.shiba.output.documentfieldpreparers;
 
-import static java.util.Optional.ofNullable;
-
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
+import java.util.Map.Entry;
 import lombok.Value;
 import org.codeforamerica.shiba.inputconditions.Condition;
 import org.codeforamerica.shiba.pages.config.PageGroupConfiguration;
@@ -40,12 +35,21 @@ public class SubworkflowIterationScopeTracker {
   private String scopeForIteration(PageGroupConfiguration pageGroupConfiguration,
       Iteration iteration) {
     Map<String, Condition> scopes = pageGroupConfiguration.getAddedScope();
-    return ofNullable(scopes).flatMap(allScopes -> allScopes.entrySet().stream()
-        .filter(entry -> ofNullable(iteration.getPagesData().get(entry.getValue().getPageName()))
-            .map(pageData -> entry.getValue().matches(pageData, iteration.getPagesData()))
-            .orElse(false))
-        .findAny()
-        .map(Map.Entry::getKey)).orElse(null);
+
+    if (scopes == null) {
+      return null;
+    }
+
+    Optional<String> result = scopes.entrySet().stream()
+        .filter(
+            entry -> Optional.ofNullable(
+                    iteration.getPagesData().get(entry.getValue().getPageName()))
+                .map(pageData -> entry.getValue().matches(pageData, iteration.getPagesData()))
+                .orElse(false)
+        ).findAny()
+        .map(Entry::getKey);
+
+    return result.orElse(null);
   }
 
   @Value
