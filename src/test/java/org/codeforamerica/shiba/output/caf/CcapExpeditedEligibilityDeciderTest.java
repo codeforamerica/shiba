fa25baf30baf8f3c7312a4ab8fad153212130575
Alex Gonzalez <spokenbird@gmail.com>
2022-02-25 14:52:14 -0800

    Add 12 years or under check to CcapExpeditedEligibilityDecider
    [#181032210]
    
    Co-authored-by: Taylor Johnson <taylor.johnson@state.mn.us>
    Co-authored-by: Ramesh Shakya <ramesh.shakya@state.mn.us>
    Co-authored-by: Touhid Khan <touhid.khan@state.mn.us>

--- src/test/java/org/codeforamerica/shiba/output/caf/CcapExpeditedEligibilityDeciderTest.java
+++ src/test/java/org/codeforamerica/shiba/output/caf/CcapExpeditedEligibilityDeciderTest.java
@@ -2,10 +2,12 @@ package org.codeforamerica.shiba.output.caf;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import org.codeforamerica.shiba.pages.data.ApplicationData;
+import org.codeforamerica.shiba.testutilities.PagesDataBuilder;
 import org.codeforamerica.shiba.testutilities.TestApplicationDataBuilder;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -13,32 +15,47 @@ import org.junit.jupiter.params.provider.CsvSource;
 class CcapExpeditedEligibilityDeciderTest {
 
   private final CcapExpeditedEligibilityDecider ccapExpeditedEligibilityDecider = new CcapExpeditedEligibilityDecider();
+  private TestApplicationDataBuilder applicationDataBuilder;
+
+  @BeforeEach
+  void setup() {
+    // Initialize with eligible snap
+    applicationDataBuilder = new TestApplicationDataBuilder()
+        .withApplicantPrograms(List.of("CCAP"))
+        .withSubworkflow("household",
+            new PagesDataBuilder().withPageData("householdMemberInfo", Map.of(
+                "firstName", "Jane",
+                "lastName", "Testerson",
+                "dateOfBirth", List.of("01", "09", "2020"))).build());
+  }
 
   @ParameterizedTest
   @CsvSource(value = {
       "HOTEL_OR_MOTEL,ELIGIBLE",
       "TEMPORARILY_WITH_FRIENDS_OR_FAMILY_DUE_TO_ECONOMIC_HARDSHIP,ELIGIBLE",
-      "TEMPORARILY_WITH_FRIENDS_OR_FAMILY_OTHER_REASONS,NOT_ELIGIBLE",
+      "TEMPORARILY_WITH_FRIENDS_OR_FAMILY_OTHER_REASONS,ELIGIBLE",
       "EMERGENCY_SHELTER,ELIGIBLE",
       "LIVING_IN_A_PLACE_NOT_MEANT_FOR_HOUSING,ELIGIBLE",
       "PAYING_FOR_HOUSING_WITH_RENT_LEASE_OR_MORTGAGE,NOT_ELIGIBLE",
-      "FOSTER_CARE_OR_GROUP_HOME,NOT_ELIGIBLE",
-      "HOSPITAL_OR_OTHER_TREATMENT_FACILITY,NOT_ELIGIBLE",
-      "JAIL_OR_JUVENILE_DETENTION_FACILITY,NOT_ELIGIBLE",
-      "UNKNOWN,NOT_ELIGIBLE",
-      "PREFER_NOT_TO_SAY,NOT_ELIGIBLE",
+      "FOSTER_CARE_OR_GROUP_HOME,ELIGIBLE",
+      "HOSPITAL_OR_OTHER_TREATMENT_FACILITY,ELIGIBLE",
+      "JAIL_OR_JUVENILE_DETENTION_FACILITY,ELIGIBLE",
+      "UNKNOWN,ELIGIBLE",
+      "PREFER_NOT_TO_SAY,ELIGIBLE",
   })
   void shouldQualifyWhenLivingSituationIsEligible(
       String livingSituation,
       CcapExpeditedEligibility expectedDecision
   ) {
-    ApplicationData applicationData = createApplicationData(List.of(livingSituation), "CCAP");
+    ApplicationData applicationData = applicationDataBuilder.withPageData("livingSituation",
+        "livingSituation", livingSituation).build();
     assertThat(ccapExpeditedEligibilityDecider.decide(applicationData)).isEqualTo(expectedDecision);
   }
 
   @Test
   void shouldBeUndeterminedWhenLivingSituationIsNotAvailable() {
-    ApplicationData applicationData = createApplicationData(Collections.emptyList(), "CCAP");
+    ApplicationData applicationData = applicationDataBuilder.withPageData("livingSituation",
+        "livingSituation", List.of()).build();
     assertThat(ccapExpeditedEligibilityDecider.decide(applicationData))
         .isEqualTo(CcapExpeditedEligibility.UNDETERMINED);
   }
@@ -51,11 +68,41 @@ class CcapExpeditedEligibilityDeciderTest {
   }
 
   @Test
-  void shouldBeNotEligibleWhenLivingWithFamilyFriendsDueToOtherReasons() {
-    ApplicationData applicationData = createApplicationData(
-        List.of("TEMPORARILY_WITH_FRIENDS_OR_FAMILY_OTHER_REASONS"), "CCAP");
+  void ShouldNotQualifyIfThereAreNoHouseholdMembersUnder12() {
+    ApplicationData applicationData = new TestApplicationDataBuilder()
+        .withApplicantPrograms(List.of("CCAP"))
+        .withPageData("livingSituation", "livingSituation", "EMERGENCY_SHELTER")
+        .withSubworkflow("household",
+            new PagesDataBuilder().withPageData("householdMemberInfo", Map.of(
+                "firstName", "Jane",
+                "lastName", "Testerson",
+                "dateOfBirth", List.of("01", "09", "1987"))),
+            new PagesDataBuilder().withPageData("householdMemberInfo", Map.of(
+                "firstName", "John",
+                "lastName", "Testerson",
+                "dateOfBirth", List.of("01", "09", "1999")))).build();
+
     assertThat(ccapExpeditedEligibilityDecider.decide(applicationData))
         .isEqualTo(CcapExpeditedEligibility.NOT_ELIGIBLE);
+  }
+
+  @Test
+  void shouldOnlyQualifyIfAtLeastOneHouseholdMemberIsAge12OrUnder() {
+    ApplicationData applicationData = new TestApplicationDataBuilder()
+        .withApplicantPrograms(List.of("CCAP"))
+        .withPageData("livingSituation", "livingSituation", "EMERGENCY_SHELTER")
+        .withSubworkflow("household",
+            new PagesDataBuilder().withPageData("householdMemberInfo", Map.of(
+                "firstName", "Jane",
+                "lastName", "Testerson",
+                "dateOfBirth", List.of("01", "09", "2020"))),
+            new PagesDataBuilder().withPageData("householdMemberInfo", Map.of(
+                "firstName", "John",
+                "lastName", "Testerson",
+                "dateOfBirth", List.of("01", "09", "1999")))).build();
+
+    assertThat(ccapExpeditedEligibilityDecider.decide(applicationData))
+        .isEqualTo(CcapExpeditedEligibility.ELIGIBLE);
   }
 
   private ApplicationData createApplicationData(List<String> livingSituation, String program) {
