d1a75970c9c214a0e22cf499eb3ccbb4766ee6ce
britneyepps <bepps@codeforamerica.org>
2022-03-10 14:48:22 -0500

    Added status of 'undeliverable' for JPEG issue

--- src/main/java/org/codeforamerica/shiba/application/Status.java
+++ src/main/java/org/codeforamerica/shiba/application/Status.java
@@ -5,7 +5,8 @@ public enum Status {
   SENDING("sending"),
   DELIVERED("delivered"),
   DELIVERY_FAILED("delivery_failed"),
-  RESUBMISSION_FAILED("resubmission_failed");
+  RESUBMISSION_FAILED("resubmission_failed"),
+  UNDELIVERABLE("undeliverable");
 
   private final String displayName;
 
@@ -20,6 +21,7 @@ public enum Status {
       case "delivered" -> DELIVERED;
       case "delivery_failed" -> DELIVERY_FAILED;
       case "resubmission_failed" -> RESUBMISSION_FAILED;
+      case "undeliverable" -> UNDELIVERABLE;
       default -> null;
     };
   }
