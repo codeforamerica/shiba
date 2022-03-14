271066518ac91176002e8e5d7cc72c916c311840
Ben Calegari <bcalegari@codeforamerica.org>
2022-03-14 14:22:16 -0700

    Make core size and queue capacity configurable in env variables
    Co-authored-by: Sree Prasad <sprasad@codeforamerica.org>
    Co-authored-by: Alex Gonzalez <agonzalez@codeforamerica.org>

--- src/main/java/org/codeforamerica/shiba/configurations/SpringAsyncConfiguration.java
+++ src/main/java/org/codeforamerica/shiba/configurations/SpringAsyncConfiguration.java
@@ -1,6 +1,7 @@
 package org.codeforamerica.shiba.configurations;
 
 import java.util.concurrent.Executor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -9,13 +10,15 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @Configuration
 @EnableAsync
 public class SpringAsyncConfiguration implements AsyncConfigurer {
+  @Value("${asyncConfiguration.corePoolSize}") int corePoolSize;
+  @Value("${asyncConfiguration.queueCapacity}") int queueCapacity;
 
   @Override
   public Executor getAsyncExecutor() {
     ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-    executor.setCorePoolSize(10);
+    executor.setCorePoolSize(corePoolSize);
     // keep the queue small to avoid excessive delay if there are ESB retries
-    executor.setQueueCapacity(2);
+    executor.setQueueCapacity(queueCapacity );
     executor.setWaitForTasksToCompleteOnShutdown(true); // avoid interruptionException
     executor.setThreadNamePrefix("AsyncExecutor-");
     executor.initialize();
