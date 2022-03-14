f6e1c1af94fe53016128dc30088b931c2f35fac8
Sree P <sprasad@codeforamerica.org>
2022-03-14 12:44:40 -0700

    Overwriting default configurations - start with 10 threads for ESB submissions (default is 1) - lower queue capacity so that more threads will spin up to complete tasks (default queue capacity is integer.max)

--- src/main/java/org/codeforamerica/shiba/configurations/SpringAsyncConfiguration.java
+++ src/main/java/org/codeforamerica/shiba/configurations/SpringAsyncConfiguration.java
@@ -1,10 +1,24 @@
 package org.codeforamerica.shiba.configurations;
 
+import java.util.concurrent.Executor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @EnableAsync
-public class SpringAsyncConfiguration {
+public class SpringAsyncConfiguration implements AsyncConfigurer {
 
+  @Override
+  public Executor getAsyncExecutor() {
+    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+    executor.setCorePoolSize(10);
+    // keep the queue small to avoid excessive delay if there are ESB retries
+    executor.setQueueCapacity(2);
+    executor.setWaitForTasksToCompleteOnShutdown(true); // avoid interruptionException
+    executor.setThreadNamePrefix("AsyncExecutor-");
+    executor.initialize();
+    return executor;
+  }
 }
