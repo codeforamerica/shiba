4c228660f2bb88d8fee2f8368951f625d073a718
Ramesh Shakya <ramesh.shakya@state.mn.us>
2022-03-29 09:26:39 -0500

    set document_name to blank by default.

--- src/main/resources/db/migration/V39__add-document-name-application-status.sql
+++ src/main/resources/db/migration/V39__add-document-name-application-status.sql
@@ -1,5 +1,5 @@
 ALTER TABLE application_status
-    ADD COLUMN document_name VARCHAR;
+    ADD COLUMN document_name VARCHAR DEFAULT '';
     
 ALTER TABLE application_statuses_audit
-    ADD COLUMN document_name VARCHAR;
+    ADD COLUMN document_name VARCHAR DEFAULT '';
