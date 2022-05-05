5f8382b482cfbdad2f119138e5fcb12192895660
Ken McCrary <kenmccrary@gmail.com>
2022-05-05 16:56:57 -0400

    State Machine 1
    [#181388566]
    
    @kmccrary

--- src/test/java/org/codeforamerica/shiba/statemachine/StateMachineTest.java
+++ src/test/java/org/codeforamerica/shiba/statemachine/StateMachineTest.java
@@ -11,7 +11,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @ActiveProfiles("test")
 public class StateMachineTest {
-
     @Autowired
     private StateMachineService service;
     private StateMachine<StatesAndEvents.DeliveryStates, StatesAndEvents.DeliveryEvents> testMachine;
