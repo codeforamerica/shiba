2bb2d0e6e8f6c80b22d51552284463ef281fd392
Ben Calegari <bcalegari@codeforamerica.org>
2022-05-05 17:03:44 -0700

    Extracted UploadedDocsPreparer
    - Download action and document consumer were both looping through docs and adding cover pages and now we have a class for it
    - Tried to simplify MnitDocumentConsumer in preparation for separating sending to Filenet and SFTP
    
    [#182087322]

--- src/main/java/org/codeforamerica/shiba/output/MnitDocumentConsumer.java
+++ src/main/java/org/codeforamerica/shiba/output/MnitDocumentConsumer.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
 import lombok.extern.slf4j.Slf4j;
 import org.codeforamerica.shiba.County;
 import org.codeforamerica.shiba.MonitoringService;
@@ -27,10 +26,7 @@ import org.codeforamerica.shiba.output.caf.FilenameGenerator;
 import org.codeforamerica.shiba.output.pdf.PdfGenerator;
 import org.codeforamerica.shiba.output.xml.XmlGenerator;
 import org.codeforamerica.shiba.pages.RoutingDecisionService;
-import org.codeforamerica.shiba.pages.config.FeatureFlagConfiguration;
 import org.codeforamerica.shiba.pages.data.ApplicationData;
-import org.codeforamerica.shiba.pages.data.UploadedDocument;
-import org.codeforamerica.shiba.pages.emails.EmailClient;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 
@@ -38,42 +34,37 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class MnitDocumentConsumer {
 
-  private final EmailClient emailClient;
   private final XmlGenerator xmlGenerator;
   private final PdfGenerator pdfGenerator;
   private final MonitoringService monitoringService;
   private final RoutingDecisionService routingDecisionService;
   private final ApplicationStatusRepository applicationStatusRepository;
-  private final FeatureFlagConfiguration featureFlagConfiguration;
   private final FilenetWebServiceClient mnitFilenetClient;
   private final FilenameGenerator filenameGenerator;
+  private final UploadedDocsPreparer uploadedDocsPreparer;
 
-  public MnitDocumentConsumer(EmailClient emailClient,
-      XmlGenerator xmlGenerator,
+  public MnitDocumentConsumer(XmlGenerator xmlGenerator,
       PdfGenerator pdfGenerator,
       MonitoringService monitoringService,
       RoutingDecisionService routingDecisionService,
       ApplicationStatusRepository applicationStatusRepository,
-      FeatureFlagConfiguration featureFlagConfiguration,
       FilenetWebServiceClient mnitFilenetClient,
-      FilenameGenerator filenameGenerator) {
+      FilenameGenerator filenameGenerator,
+      UploadedDocsPreparer uploadedDocsPreparer) {
     this.xmlGenerator = xmlGenerator;
     this.pdfGenerator = pdfGenerator;
     this.monitoringService = monitoringService;
     this.routingDecisionService = routingDecisionService;
     this.applicationStatusRepository = applicationStatusRepository;
-    this.emailClient = emailClient;
-    this.featureFlagConfiguration = featureFlagConfiguration;
     this.mnitFilenetClient = mnitFilenetClient;
     this.filenameGenerator = filenameGenerator;
+    this.uploadedDocsPreparer = uploadedDocsPreparer;
   }
 
   public void processCafAndCcap(Application application) {
-    String id = application.getId();
-    monitoringService.setApplicationId(id);
-
+    monitoringService.setApplicationId(application.getId());
     // Send the CAF, CCAP, and XML files in parallel
-    List<Thread> threads = createThreadsForSendingThisApplication(application, id);
+    List<Thread> threads = createThreadsForSendingThisApplication(application, application.getId());
 
     // Wait for everything to finish before returning
     threads.forEach(thread -> {
@@ -89,7 +80,6 @@ public class MnitDocumentConsumer {
   private List<Thread> createThreadsForSendingThisApplication(Application application, String id) {
     List<Thread> threads = new ArrayList<>();
     Set<RoutingDestination> allRoutingDestinations = new HashSet<>();
-
     ApplicationData applicationData = application.getApplicationData();
 
     // Create threads for sending CAF & CCAP pdfs to each recipient
@@ -97,16 +87,14 @@ public class MnitDocumentConsumer {
       List<RoutingDestination> routingDestinations =
           routingDecisionService.getRoutingDestinations(applicationData, doc);
 
-      // Keep track of routing destinations so we know who to send the XML to later
+      // Keep track of routing destinations so that we know who to send the XML to later
       allRoutingDestinations.addAll(routingDestinations);
 
       for (RoutingDestination rd : routingDestinations) {
         ApplicationFile pdf = pdfGenerator.generate(id, doc, CASEWORKER, rd);
-        Thread thread = new Thread(() -> sendFileAndUpdateStatus(application, doc, pdf, rd));
+        Thread thread = new Thread(
+            () -> sendOrSetToFailed(application, rd, pdf, doc));
         thread.start();
-        log.info(
-            "Started Thread for: " + doc.name() + " with filename: " + pdf.getFileName() + " to: "
-                + rd.getName());
         threads.add(thread);
       }
     });
@@ -114,7 +102,8 @@ public class MnitDocumentConsumer {
     // Create threads for sending the xml to each recipient who also received a PDF
     allRoutingDestinations.forEach(rd -> {
       ApplicationFile xml = xmlGenerator.generate(id, CAF, CASEWORKER);
-      Thread thread = new Thread(() -> sendFile(application, XML, xml, rd));
+      Thread thread = new Thread(() -> mnitFilenetClient.send(xml, rd, application.getId(), XML,
+          application.getFlow()));
       thread.start();
       threads.add(thread);
     });
@@ -122,8 +111,11 @@ public class MnitDocumentConsumer {
   }
 
   public void processUploadedDocuments(Application application) {
-    List<ApplicationFile> uploadedDocs = prepareUploadedDocsForSending(application);
-    if (uploadedDocs.isEmpty()) {
+    List<ApplicationFile> applicationFiles = uploadedDocsPreparer.prepare(
+        application.getApplicationData().getUploadedDocs(),
+        application);
+
+    if (applicationFiles.isEmpty()) {
       log.error(
           "There was an issue processing and delivering uploaded documents. Reach out to client to upload again.");
       applicationStatusRepository.createOrUpdateAllForDocumentType(application,
@@ -136,92 +128,42 @@ public class MnitDocumentConsumer {
     for (RoutingDestination routingDestination : routingDestinations) {
       boolean sendXMLToDakota = routingDestination.getName().equals(County.Dakota.name())
           && application.getFlow() == FlowType.LATER_DOCS;
+
       if (sendXMLToDakota) {
         ApplicationFile xml = xmlGenerator.generate(application.getId(), XML, CASEWORKER);
-        sendFileAndUpdateStatus(application, XML, xml, routingDestination);
+        sendOrSetToFailed(application, routingDestination, xml, XML);
       }
-      log.info("Uploaded docs to submit %s".formatted(uploadedDocs.size()));
-     for (int i = 0; i < uploadedDocs.size(); i++) {
-        ApplicationFile uploadedDoc = uploadedDocs.get(i);
+
+      log.info("Uploaded docs to submit %s".formatted(applicationFiles.size()));
+     for (int i = 0; i < applicationFiles.size(); i++) {
+        ApplicationFile uploadedDoc = applicationFiles.get(i);
         // rename file with filename that is specific to this destination
         String extension = Utils.getFileType(uploadedDoc.getFileName());
         String newFilename = filenameGenerator.generateUploadedDocumentName(application, i,
-            extension, routingDestination, uploadedDocs.size());
+            extension, routingDestination, applicationFiles.size());
         ApplicationFile renamedFile = new ApplicationFile(uploadedDoc.getFileBytes(),
             newFilename);
-        sendFileAndUpdateStatus(application, UPLOADED_DOC, renamedFile, routingDestination);
+
+        sendOrSetToFailed(application, routingDestination, renamedFile, UPLOADED_DOC);
       }
     }
   }
 
-  /**
-   * Returns a list of uploaded docs that have been renamed to meet the standards of MNIT, converted
-   * to PDFs (when possible), and have had cover pages added (when possible) Note: the filenames for
-   * these documents will include the county dhsProviderId. Those filenames are changed later to
-   * include the dhsProviderId specific to whatever RoutingDestination the file is being sent to
-   */
-  private List<ApplicationFile> prepareUploadedDocsForSending(Application application) {
-    var uploadedDocs = application.getApplicationData().getUploadedDocs();
-    List<ApplicationFile> applicationFiles = new ArrayList<>();
-    byte[] coverPage = pdfGenerator.generateCoverPageForUploadedDocs(application);
-    for (int i = 0; i < uploadedDocs.size(); i++) {
-      UploadedDocument originalDocument = uploadedDocs.get(i);
-      try {
-        ApplicationFile preparedDocument =
-            pdfGenerator.generateForUploadedDocument(originalDocument, i, application, coverPage);
-        if (preparedDocument != null && preparedDocument.getFileBytes().length > 0) {
-          log.info("Now queueing file to send: %s".formatted(preparedDocument.getFileName()));
-          applicationFiles.add(preparedDocument);
-        } else {
-          // This should only happen in a dev environment
-          log.error("Skipped uploading file %s because it was empty."
-              .formatted(originalDocument.getFilename()));
-        }
-      } catch (NullPointerException e) {
-        log.warn("Null Pointer Exception Caught while preparing document "+ originalDocument.getSysFileName()+" to send " + e.getMessage());
-        ApplicationFile preparedDocument =
-            pdfGenerator.generateForUploadedDocument(originalDocument, i, application, null);
-        if (preparedDocument != null && preparedDocument.getFileBytes().length > 0) {
-          log.info("Now queueing file to send: %s".formatted(preparedDocument.getFileName()) +" without cover page");
-          applicationFiles.add(preparedDocument);
-        } else {
-          // This should only happen in a dev environment
-          log.error("Skipped uploading file %s because it was empty."
-              .formatted(originalDocument.getFilename()));
-        }
-        continue;
-      }
-    }
-    return applicationFiles;
-  }
-
-  private void sendFileAndUpdateStatus(Application application, Document documentType,
-      ApplicationFile applicationFile, RoutingDestination routingDestination) {
-
-    String id = application.getId();
+  private void sendOrSetToFailed(Application application, RoutingDestination routingDestination,
+      ApplicationFile renamedFile, Document document) {
     try {
-      applicationStatusRepository.createOrUpdate(id, documentType, routingDestination.getName(),
-          SENDING,applicationFile.getFileName());
-      sendFile(application, documentType, applicationFile, routingDestination);
+      applicationStatusRepository.createOrUpdate(application.getId(), document,
+          routingDestination.getName(),
+          SENDING, renamedFile.getFileName());
+      mnitFilenetClient.send(renamedFile, routingDestination, application.getId(), document,
+          application.getFlow());
     } catch (Exception e) {
-      applicationStatusRepository.createOrUpdate(id, documentType, routingDestination.getName(),
-          DELIVERY_FAILED,applicationFile.getFileName());
+      applicationStatusRepository.createOrUpdate(application.getId(), document,
+          routingDestination.getName(),
+          DELIVERY_FAILED, renamedFile.getFileName());
       log.error("Failed to send document %s to recipient %s for application %s with error, "
-          .formatted(documentType, routingDestination.getName(), id), e);
+          .formatted(document, routingDestination.getName(), application.getId()), e);
     }
   }
 
-  private void sendFile(Application application, Document document, ApplicationFile file,
-      RoutingDestination routingDestination) {
-
-    // This is where we want to generate the new filename
-    log.info("Now sending %s to recipient %s for application %s".formatted(
-        document.name(),
-        routingDestination.getName(),
-        application.getId()));
-
-    mnitFilenetClient.send(file, routingDestination, application.getId(), document,
-        application.getFlow());
-
-  }
 }
