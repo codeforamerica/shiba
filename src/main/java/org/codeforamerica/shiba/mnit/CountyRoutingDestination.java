20fd38913ef07ad34b3bb286aaa5d88b9dd2e101
Ben Calegari <bcalegari@codeforamerica.org>
2022-06-13 11:38:31 -0700

    Set NPIs for all CountyRoutingDestinations
    - Counties must have at least dhs provider ids, emails, and phone numbers (address optional) no more no-args constructor leading to weird null's in application IDs
    - Simplify env set up to be either prod or non-prod, the only difference should be the NPIs and help emails
    
    [#182255304]

--- src/main/java/org/codeforamerica/shiba/mnit/CountyRoutingDestination.java
+++ src/main/java/org/codeforamerica/shiba/mnit/CountyRoutingDestination.java
@@ -1,26 +1,28 @@
 package org.codeforamerica.shiba.mnit;
 
-import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 import org.codeforamerica.shiba.County;
 import org.codeforamerica.shiba.pages.enrichment.Address;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
-@NoArgsConstructor
 public class CountyRoutingDestination extends RoutingDestination {
 
   private County county;
   private Address postOfficeAddress;
 
-  @Builder
   public CountyRoutingDestination(County county, String dhsProviderId,
       String email, String phoneNumber, Address postOfficeAddress) {
     super(dhsProviderId, email, phoneNumber);
     this.county = county;
     this.postOfficeAddress = postOfficeAddress;
+  }
+
+  public CountyRoutingDestination(County county, String dhsProviderId, String email,
+      String phoneNumber) {
+    super(dhsProviderId, email, phoneNumber);
+    this.county = county;
   }
 
   @Override
