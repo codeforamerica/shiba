006601756be52df46044837225896d3a7fdbb5a8
Ramesh Shakya <ramesh.shakya@state.mn.us>
2022-12-05 13:45:00 -0600

    Later Docs Confirmation email
    [#183534356]

--- src/main/java/org/codeforamerica/shiba/application/parsers/CountyParser.java
+++ src/main/java/org/codeforamerica/shiba/application/parsers/CountyParser.java
@@ -1,6 +1,7 @@
 package org.codeforamerica.shiba.application.parsers;
 
 import static org.codeforamerica.shiba.application.parsers.ApplicationDataParser.Field.IDENTIFY_COUNTY;
+import static org.codeforamerica.shiba.application.parsers.ApplicationDataParser.Field.IDENTIFY_COUNTY_LATER_DOCS;
 import static org.codeforamerica.shiba.application.parsers.ApplicationDataParser.getFirstValue;
 
 import lombok.extern.slf4j.Slf4j;
@@ -14,9 +15,10 @@ public class CountyParser {
 
   public static County parse(ApplicationData applicationData) {
     String countyName = getFirstValue(applicationData.getPagesData(), IDENTIFY_COUNTY);
+    String countyNameLD = getFirstValue(applicationData.getPagesData(), IDENTIFY_COUNTY_LATER_DOCS);
 
     try {
-      return County.getForName(countyName);
+      return County.getForName(!countyNameLD.equalsIgnoreCase("Other")?countyNameLD:countyName);
     } catch (Exception e) {
       log.warn("Could not retrieve County object corresponding to county name: " + countyName, e);
       return County.Other;
