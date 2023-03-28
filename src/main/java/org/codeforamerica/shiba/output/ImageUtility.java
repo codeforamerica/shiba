9a086289115622b8e6eddb2e951c89d31c755e8f
pwjmb05 <john.bisek@state.mn.us>
2023-03-28 17:42:00 -0500

    Uploaded PDF is all black on the combined PDF
    [#184329657]
    
    Co-authored-by: Eric Johnson <eric.m.johnson@state.mn.us>
    Co-authored-by: Touhid Khan <Touhid.Khan@state.mn.us>
    Co-authored-by: Ramesh Shakya <ramesh.shakya@state.mn.us>
    Co-authored-by: Taylor Johnson <taylor.johnson@state.mn.us>

--- src/main/java/org/codeforamerica/shiba/output/ImageUtility.java
+++ src/main/java/org/codeforamerica/shiba/output/ImageUtility.java
@@ -48,6 +48,11 @@ public class ImageUtility {
 		}
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 		doc.save(outputStream);
+		
+		// Modifying the original logic, adding this close() request as described
+		// in the documentation for PDDocument
+		doc.close();
+		
 		return outputStream.toByteArray();
 
 	}
@@ -68,14 +73,20 @@ public class ImageUtility {
 			final ByteArrayOutputStream baos = new ByteArrayOutputStream();
 			imgWriter.setOutput(ImageIO.createImageOutputStream(baos));
 			BufferedImage bi = img.getImage();
-			IIOImage iioi;
-			if (bi.getTransparency() == BufferedImage.OPAQUE) {
-				iioi = new IIOImage(bi, null, null);
-			} else if (bi.getTransparency() == BufferedImage.TRANSLUCENT) {
-				iioi = new IIOImage(img.getOpaqueImage(), null, null);
-			} else {
-				iioi = new IIOImage(img.getOpaqueImage(), null, null);
-			}
+			
+			// Modifying the original logic to not change the image transparency property.
+			// Original logic is left commented out.
+			// Changing a translucent image to opaque causes the image to appear black.
+			IIOImage iioi = new IIOImage(bi, null, null);
+			//IIOImage iioi;
+			//if (bi.getTransparency() == BufferedImage.OPAQUE) {
+			//	iioi = new IIOImage(bi, null, null);
+			//} else if (bi.getTransparency() == BufferedImage.TRANSLUCENT) {
+			//	iioi = new IIOImage(img.getOpaqueImage(), null, null);
+			//} else {
+			//	iioi = new IIOImage(img.getOpaqueImage(), null, null);
+			//}
+			
 			imgWriter.write(null, iioi, iwp);
 			final ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
 			final PDImageXObject imgNew = JPEGFactory.createFromStream(doc, bais);
