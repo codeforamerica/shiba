510bbbeb1bfa3eaad04ad26d1a370c55145db224
John Bisek <78550461+BisekJM@users.noreply.github.com>
2023-04-18 14:09:40 -0500

    Hc renewal accessibility (#278)
    * Add HEALTHCARE_RENEWAL flow accessibility tests
    
    [#184907287]
    
    Co-authored-by: Eric Johnson <eric.m.johnson@state.mn.us>
    Co-authored-by: Touhid Khan <Touhid.Khan@state.mn.us>
    Co-authored-by: Ramesh Shakya <ramesh.shakya@state.mn.us>
    Co-authored-by: Taylor Johnson <taylor.johnson@state.mn.us>

--- src/test/java/org/codeforamerica/shiba/journeys/AccessibilityJourneyTest.java
+++ src/test/java/org/codeforamerica/shiba/journeys/AccessibilityJourneyTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 import static org.codeforamerica.shiba.testutilities.YesNoAnswer.NO;
 import static org.codeforamerica.shiba.testutilities.YesNoAnswer.YES;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -15,14 +17,16 @@ import org.codeforamerica.shiba.testutilities.AccessibilityTestPage;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import com.deque.html.axecore.results.Results;
 import com.deque.html.axecore.results.Rule;
 import com.deque.html.axecore.selenium.AxeBuilder;
 
-//@Tag("a11y")
+@Tag("a11y")
 public class AccessibilityJourneyTest extends JourneyTest {
 
   protected static List<Rule> resultsList = new ArrayList<>();
@@ -141,6 +145,108 @@ public class AccessibilityJourneyTest extends JourneyTest {
     uploadPdfFile();
     await().until(uploadCompletes());
     testPage.clickButton("Submit my documents");
+  }
+  
+  @Test
+  void healthcareRenewalFlow() {
+    when(featureFlagConfiguration.get("submit-via-api")).thenReturn(FeatureFlag.ON);
+    when(featureFlagConfiguration.get("tribal-routing")).thenReturn(FeatureFlag.ON);
+    navigateTo("healthcareRenewalUpload");
+    assertThat(driver.getTitle()).isEqualTo("Health Care Renewal Document Upload");
+
+    // should allow me to enter personal info and continue the flow if my county is supported
+    testPage.enter("county", "Select your county");
+    testPage.enter("tribalNation", "Select a Tribal Nation");
+    testPage.clickContinue();
+    assertThat(driver.getTitle()).isEqualTo("Health Care Renewal Document Upload");
+    testPage.enter("county", "Hennepin");
+    testPage.enter("tribalNation", "White Earth Nation");
+    testPage.clickContinue();
+    assertThat(driver.getTitle()).isEqualTo("Health Care Renewal Document Upload");
+    testPage.enter("county", "Hennepin");
+    testPage.enter("tribalNation", "Select a Tribal Nation");
+    testPage.clickContinue();
+
+    assertThat(driver.getTitle()).isEqualTo("Match Info");
+    testPage.enter("firstName", "defaultFirstName");
+    testPage.enter("lastName", "defaultLastName");
+    testPage.enter("ssn", "123456789");
+    testPage.enter("caseNumber", "123456789");//9 digits will cause error
+    testPage.enter("phoneNumber", "7041234567");
+    assertThat(testPage.getHeader()).isEqualTo("Before you start, we need to match your documents to your health care case");
+    testPage.clickContinue();
+    assertThat(driver.getTitle()).isEqualTo("Match Info");//stays on match info page
+    testPage.enter("caseNumber", "123");//too short
+    testPage.clickContinue();
+    assertThat(driver.getTitle()).isEqualTo("Match Info");//stays on match info page
+    testPage.enter("caseNumber", "12345678");
+    testPage.clickContinue();
+    testPage.clickContinue();
+
+    // should allow me to upload documents and those documents should be sent to the ESB
+    assertThat(driver.getTitle()).isEqualTo("Upload documents");
+    assertThat(driver.findElements(By.className("reveal")).size()).isEqualTo(0);
+
+    uploadPdfFile();
+    waitForDocumentUploadToComplete();
+    testPage.clickButton("Submit my documents");
+    assertThat(driver.getTitle()).isEqualTo("Doc submit confirmation");
+    testPage.clickButton("No, add more documents"); // Go back
+    assertThat(driver.getTitle()).isEqualTo("Upload documents");
+
+    testPage.clickButton("Submit my documents");
+    testPage.clickButton("Yes, submit and finish");
+    assertThat(driver.getTitle()).isEqualTo("Documents Sent");
+    verify(pageEventPublisher).publish(any());
+
+    // Assert that applicant can't resubmit docs at this point
+    navigateTo("uploadDocuments");
+    assertThat(driver.getTitle()).isEqualTo("Documents Sent");
+
+    navigateTo("documentSubmitConfirmation");
+    assertThat(driver.getTitle()).isEqualTo("Documents Sent");
+    
+    // repeat renewal flow to verify another session has been created
+    navigateTo("healthcareRenewalUpload");
+    assertThat(driver.getTitle()).isEqualTo("Health Care Renewal Document Upload");
+    
+    WebElement selectedOption = testPage.getSelectedOption("county");
+    assertThat(selectedOption.getText()).isEqualTo("Select your county");
+    testPage.enter("county", "Hennepin");
+    testPage.clickContinue();
+
+    assertThat(driver.getTitle()).isEqualTo("Match Info");
+    testPage.enter("firstName", "defaultFirstName");
+    testPage.enter("lastName", "defaultLastName");
+    testPage.enter("ssn", "123456789");
+    testPage.enter("phoneNumber", "7041234567");
+    assertThat(testPage.getHeader()).isEqualTo("Before you start, we need to match your documents to your health care case");
+    testPage.enter("caseNumber", "12345678");
+    testPage.clickContinue();
+    testPage.clickContinue();
+    navigateTo("healthcareRenewalUpload");
+    WebElement selectedOption2 = testPage.getSelectedOption("county");
+    assertThat(selectedOption2.getText()).isEqualTo("Hennepin");
+    testPage.clickContinue();
+    testPage.clickContinue();
+    testPage.clickContinue();
+    // should allow me to upload documents and those documents should be sent to the ESB
+    assertThat(driver.getTitle()).isEqualTo("Upload documents");
+    assertThat(driver.findElements(By.className("reveal")).size()).isEqualTo(0);
+
+    uploadPdfFile();
+    waitForDocumentUploadToComplete();
+    testPage.clickButton("Submit my documents");
+    assertThat(driver.getTitle()).isEqualTo("Doc submit confirmation");
+    testPage.clickButton("Yes, submit and finish");
+    assertThat(driver.getTitle()).isEqualTo("Documents Sent");
+
+    // Assert that applicant can't resubmit docs at this point
+    navigateTo("uploadDocuments");
+    assertThat(driver.getTitle()).isEqualTo("Documents Sent");
+
+    navigateTo("documentSubmitConfirmation");
+    assertThat(driver.getTitle()).isEqualTo("Documents Sent");
   }
 
   @Test
