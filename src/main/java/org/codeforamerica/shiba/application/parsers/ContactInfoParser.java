39c95d43879a429c792281dd66f87d8d4875e204
touhidk <touhid.khan@state.mn.us>
2023-06-07 12:05:24 -0500

    Added helper methods to ContactInfoParser
    [#185354991]
    
    code reviewed by: eric.m.johnson@state.mn.us

--- src/main/java/org/codeforamerica/shiba/application/parsers/ContactInfoParser.java
+++ src/main/java/org/codeforamerica/shiba/application/parsers/ContactInfoParser.java
@@ -9,4 +9,25 @@ public class ContactInfoParser {
         .safeGetPageInputValue("contactInfo", "phoneOrEmail")
         .contains("EMAIL");
   }
+  
+  public static boolean optedIntoTEXT(ApplicationData applicationData) {
+    return applicationData.getPagesData()
+        .safeGetPageInputValue("contactInfo", "phoneOrEmail")
+        .contains("TEXT");
+  }
+  
+  public static String phoneNumber(ApplicationData applicationData) {
+    return applicationData.getPagesData()
+        .getPageInputFirstValue("contactInfo", "phoneNumber");
+  }
+  
+  public static String email(ApplicationData applicationData) {
+    return applicationData.getPagesData()
+        .getPageInputFirstValue("contactInfo", "email");
+  }
+  
+  public static String firstName(ApplicationData applicationData) {
+        return applicationData.getPagesData()
+            .getPageInputFirstValue("personalInfo", "firstName");
+      }
 }
