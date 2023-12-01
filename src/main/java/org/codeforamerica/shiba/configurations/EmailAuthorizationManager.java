007aa90fbbb26a32367bd1b30b8e8f6f68210088
Bshamim <shamim.babirye@state.mn.us>
2023-12-01 16:49:10 -0600

    CCM 118142 provide Shamim download PDF permissions

--- src/main/java/org/codeforamerica/shiba/configurations/EmailAuthorizationManager.java
+++ src/main/java/org/codeforamerica/shiba/configurations/EmailAuthorizationManager.java
@@ -29,7 +29,10 @@ public class EmailAuthorizationManager implements AuthorizationManager<RequestAu
 		      "marlene.merwarth@state.mn.us",
 		      "ryan.b.smith@state.mn.us",
 		      "michael.hauck@state.mn.us",
-		      "bernadette.shearer@state.mn.us"
+		      "bernadette.shearer@state.mn.us",
+		      "abdu.esmael@state.mn.us",
+		      "shamim.babirye@state.mn.us"
+		      
 		  );
 
 			@Override
